8d62e7cbe1aea08750aaeb4318a18f3eae232e91
BhekiNdhlovu <61876091+BhekiNdhlovu@users.noreply.github.com>
2021-05-26 22:13:40 +0200

    def

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,20 @@
-// import splash from '/public/SplashpageMedia/Splash.mp4';
 import './App.css';
 
 function App() {
   return (
-      <section className="header">
-      <video autoPlay loop className="video-background" muted plays-inline>
-          <source src="/SplashpageMedia/Splash.mp4" type="video/mp4"/>
-      </video>
-      <div className="welcome-message">
-          <h1>Welcome to Crypto</h1>
-          <h2>Crypto is the easiest place to track cryptocurrency.</h2>
-          <br/>
-          <br/>
-          <a href="#" className="btn btn-login">Login</a>
-          <a href="#" className="btn btn-register">Sign Up</a>
-      </div>
-      </section>
+    <section className="header">
+        {/*<video autoPlay loop className="video-background" muted plays-inline>*/}
+        {/*    <source src="/SplashpageMedia/Splash.mp4" type="video/mp4"/>*/}
+        {/*</video>*/}
+        {/*<div className="welcome-message">*/}
+        {/*    <h1>Welcome to Crypto</h1>*/}
+        {/*    <h2>Crypto is the easiest place to track cryptocurrency.</h2>*/}
+        {/*    <br/>*/}
+        {/*        <br/>*/}
+        {/*            <a href="#" className="btn btn-login">Login</a>*/}
+        {/*            <a href="#" className="btn btn-register">Sign Up</a>*/}
+        {/*</div>*/}
+    </section>
   );
 }
 

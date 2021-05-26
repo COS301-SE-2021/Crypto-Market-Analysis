cb28202559c1b23815caed724e81d21d89f08829
BhekiNdhlovu <61876091+BhekiNdhlovu@users.noreply.github.com>
2021-05-26 19:52:16 +0200

    splash page

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
-import logo from './logo.svg';
+// import splash from '/public/SplashpageMedia/Splash.mp4';
 import './App.css';
 
 function App() {
   return (
-   <h1>hello world</h1>
+      <section className="header">
+      <video autoPlay loop className="video-background" muted plays-inline>
+          <source src="/SplashpageMedia/Splash.mp4" type="video/mp4"/>
+      </video>
+      <div className="welcome-message">
+          <h1>Welcome to Crypto</h1>
+          <h2>Crypto is the easiest place to track cryptocurrency.</h2>
+          <br/>
+          <br/>
+          <a href="#" className="btn btn-login">Login</a>
+          <a href="#" className="btn btn-register">Sign Up</a>
+      </div>
+      </section>
   );
 }
 

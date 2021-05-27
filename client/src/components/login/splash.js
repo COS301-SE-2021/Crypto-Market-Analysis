e623cf48b1ba34f1f06632a8a60d05b50663c4d4
BhekiNdhlovu <61876091+BhekiNdhlovu@users.noreply.github.com>
2021-05-27 18:02:52 +0200

    splash

---
+++ client/src/components/login/splash.js
@@ -0,0 +1,22 @@
+import React from "react";
+import './css/splash.css';
+import {Link} from "react-router-dom";
+
+function splash(){
+    return(
+        <section className="header">
+            <video autoPlay loop className="video-background" muted plays-inline>
+                <source src={window.location.origin+'/Splash/Splash.mp4'} type="video/mp4"/>
+            </video>
+            <div className="welcome-message">
+                <h1>Welcome to Crypto</h1>
+                <h2>Crypto is the easiest place to track cryptocurrency.</h2>
+                <br/>
+                    <br/>
+                        <a href="/login" className="btn btn-login">Login</a>
+                        <a href="/register" className="btn btn-register">Sign Up</a>
+            </div>
+        </section>
+    );
+}
+export default splash;

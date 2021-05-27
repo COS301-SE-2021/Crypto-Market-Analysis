b77777b2f6da187d52ca4c0959e86064f2426718
Melussi Mkhonta <u14079412@tuks.co.za>
2021-05-28 01:50:53 +0200

    Added login use case and created user schema

--- server/server.js
+++ server/server.js
@@ -1,51 +1,45 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-
-const app = express();
-
-var alternate = {
-    origin: "http://localhost:8090"
-};
-
-app.use(cors(alternate));
-
-//response with json
-app.use(bodyParser.json());
-
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.get("/", (req, res) => {
-    res.json({ message: "Response to the user" });
-});
-
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`);
-});
-=======
 /*setting up express.js which will be user by the server
 to capture the user info/request made by the front-end code*/
 
-const exp = require('express')
-const path = require('path')
-const bodyParser = require('body-parser')
+const mongoose = require('mongoose');
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const User = require('./model/user');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
-mongoose.connect('mongodb://localhost:27017/register-app-db',{
+const secret_token = 'kabdaskjndbjhbkjaishouvhadjkljaosiuiygm';
+
+mongoose.connect('mongodb://localhost:27017/login-app-db',{
 
 
-})
-const app = exp()
+});
+const app = express();
 
-app.use('/', exp.static(path.join(_dirname, 'static')))
-app.use(bodyParser.json())
+app.use('/', express.static(path.join(_dirname, 'static')));
+app.use(bodyParser.json());
 
-app.post('/api/pages/register', (request, response) => {
-    console.log(request.body)
-    response.json({status: 'ok'})
-})
+app.post('/api/pages/login', async (request, response) => {
+    const {userName, password: plainTextPassword } = request.body;
+    const user = await User.find({userName}).lean();
+
+    if(!user){
+        return response.json({status: 'error', error: 'Invalid username/password entered'})
+    }
+
+    if(!plainTextPassword)
+
+    if(await bcrypt.compare(plainTextPassword, user.password)){
+        //password and username match an existing user
+
+        const token = jwt.sign({id: user.ID, username: user.username}, secret_token);
+        return response.json({status: 'ok', data: token})
+    }
+    response.json({status: 'error', error: 'Invalid username/password entered'})
+});
 
 app.listen(9999, ()=> {
 
     console.log('Server ports go up to 9999')
-})
+});

a57db2ded7a790be635a139d1b4c735f017adb6b
BhekiNdhlovu <61876091+BhekiNdhlovu@users.noreply.github.com>
2021-05-28 09:43:14 +0200

    changed splash video

--- client/src/components/login/splash.js
+++ client/src/components/login/splash.js
@@ -6,15 +6,15 @@ function splash(){
     return(
         <section className="header">
             <video autoPlay loop className="video-background" muted plays-inline>
-                <source src={window.location.origin+'/Splash/Splash.mp4'} type="video/mp4"/>
+                <source src={window.location.origin+'/Splash/splash2.mp4'} type="video/mp4"/>
             </video>
             <div className="welcome-message">
                 <h1>Welcome to Crypto</h1>
                 <h2>Crypto is the easiest place to track cryptocurrency.</h2>
                 <br/>
-                    <br/>
-                        <a href="/login" className="btn btn-login">Login</a>
-                        <a href="/register" className="btn btn-register">Sign Up</a>
+                <br/>
+                <a href="/login" className="btn btn-login">Login</a>
+                <a href="/register" className="btn btn-register">Sign Up</a>
             </div>
         </section>
     );

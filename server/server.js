ed2a509ceb94a4b1a6da9de937a1d93ec18ca123
Zee864 <82458469+Zee864@users.noreply.github.com>
2021-05-30 23:13:26 +0200

    Added concurrent server and client links

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const app = require('./app');
 const dotenv = require('dotenv');
 dotenv.config();
-const port = process.env.port || 5000;
+const port = process.env.port || 8080;
 console.log(`Listening on port ${port}`)
 const server = http.createServer(app);
 

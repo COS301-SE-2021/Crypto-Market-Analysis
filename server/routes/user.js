818007956d8d801cbddd0d73a9188d75e2d16d92
Melussi Mkhonta <u14079412@tuks.co.za>
2021-06-02 00:03:31 +0200

    Updated user.js with express router instead of express app

--- server/routes/user.js
+++ server/routes/user.js
@@ -15,10 +15,10 @@ const secret_token = 'kabdaskjndbjhbkjaishouvhadjkljaosiuiygm';
 
 //});
 
-const app = express();
+//const app = express();
 
-app.use('/', express.static(path.join(_dirname, 'static')));
-app.use(bodyParser.json());
+router.use('/', express.static(path.join(_dirname, 'static')));
+router.use(bodyParser.json());
 
 /**
  * use post method to  perform http request
@@ -27,33 +27,33 @@ app.use(bodyParser.json());
  * @param request request sent to server
  * @param response response received from server
  */
-app.post('/api/updatePassword', async (request, response) => {
+router.post("/updatePassword", async (request, response, next) => {
 
-    const {token, newPassword: plainTextPassword} = request.body;
+    let {token, newPassword: plainTextPassword} = request.body;
 
     if(!plainTextPassword || typeof plainTextPassword !== 'string')
     {
-        return response.json({status: 'error', error: 'Invalid password'})
+        return response.status(400).json({status: 'error', error: 'Invalid password'})
     }
 
     if(plainTextPassword.length < 4)
     {
-        return response.json({status: 'error', error: 'Password too short. Password should be atleast 5 characters'})
+        return response.status(400).json({status: 'error', error: 'Password too short. Password should be atleast 5 characters'})
     }
 
     try {
-        const user = jwt.verify(token, secret_token);
+        let user = jwt.verify(token, secret_token);
 
-        const _id = user.id;
+        let _id = user.id;
 
-        const password = await bcrypt.hash(plainTextPassword, 10);
+        let password = await bcrypt.hash(plainTextPassword, 10);
 
         await User.updateOne({_id}, {
             $set: {password}
         });
-        response.json({status: 'ok'})
+        response.status(200).json({status: 'ok'})
     }catch(error){
-        response.json({status: 'error',error:';))'});
+        response.status(500).json({status: 'error',error:';))'});
 
 
     }
@@ -67,23 +67,23 @@ app.post('/api/updatePassword', async (request, response) => {
  * @param request request sent to server
  * @param response response received from server
  */
-app.post('/api/login', async (request, response) => {
-    const {userName, password: plainTextPassword } = request.body;
-    const user = await User.find({userName}).lean();
+router.post("/login", async (request, response, next) => {
+    let {userName, password: plainTextPassword } = request.body;
+    let user = await User.find({userName}).lean();
 
     if(!user){
-        return response.json({status: 'error', error: 'Invalid username/password entered'})
+        return response.status(400).json({status: 'error', error: 'Invalid username/password entered'});
     }
 
-    if(!plainTextPassword)
-
-        if(await bcrypt.compare(plainTextPassword, user.password)){
+    if(!plainTextPassword) {
+        if (await bcrypt.compare(plainTextPassword, user.password)) {
             //password and username match an existing user
 
-            const token = jwt.sign({id: user.ID, username: user.userName}, secret_token);
-            return response.json({status: 'ok', data: token})
+            const token = jwt.sign({id: user.id, username: user.userName}, secret_token);
+            return response.status(200).json({status: 'ok', data: token})
         }
-    response.json({status: 'error', error: 'Invalid username/password entered'})
+    }
+    response.status(500).json({status: 'error', error: 'Invalid username/password entered'})
 });
 
 module.exports = router;

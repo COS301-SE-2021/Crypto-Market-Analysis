a01096aff0ee7451d17e043508632540c51b32f4
kg <kgothalang451@gmail.com>
2021-06-03 01:03:57 +0200

    Update PlatformSelector.js

--- client/src/components/Home/PlatformSelector/PlatformSelector.js
+++ client/src/components/Home/PlatformSelector/PlatformSelector.js
@@ -1,76 +1,83 @@
-// import "bootstrap/dist/css/bootstrap.css";
-// import {Icon} from "coinmarketcap-cryptocurrency-icons";
-// import {CheckCircle, CloseRounded} from "@material-ui/icons";
-//
-// import React, {Component} from "react";
-// import "./PlatformSelector.css"
-//
-// const cryptos = [{Name:"Bitcoin",Code:"btc"},
-//     {Name:"Litecoin",Code:"ltc"},
-//     {Name:"Etherium",Code:"eth"},
-//     {Name:"Ripple",Code:"xrp"},
-//     {Name:"Tether",Code:"usdt"}
-//
-//     ];
-// class Selector extends React.Component{
-//     constructor(props) {
-//         super(props);
-//     this.select = this.select.bind(this);
-//     this.state = {
-//             cryptList: [...cryptos.map(crypto =>{ return {...crypto,selected:false} })],
-//             j:[]
-//         };
-//     }
-//     select(index){
-//         this.setState({
-//             cryptList: [...this.state.cryptList.map((crypt,currIndex)=>{
-//                 if(index==currIndex){
-//                     crypt.selected = !crypt.selected;
-//                 };
-//                 return {
-//                     ...crypt
-//                 }
-//             })]
-//         });
-//     }
-//
-//     render() {
-//         const cryptoList = this.state;
-//         console.log(cryptoList);
-//         return(
-//             <div className="container">
-//                 <div className="nav-header">
-//                     <nav className="navbar bg-light justify-content-between">
-//                         <span className="navbar-brand navbar-brand-centered">AppName</span>
-//                         <ul className="navbar-nav">
-//                             <li className="nav-item">
-//                                 <CloseRounded/>{/*Add a button for option when account is clicked*/}
-//                             </li>
-//                         </ul>
-//                     </nav>
-//                 </div>
-//
-//                 <div className="row">
-//
-//                     {
-//                         cryptoList.cryptList.map((crypto,index) => {
-//                             return(
-//                                 <div key={crypto.Code} className="col-md-4" onClick={()=>{this.select(index)}}>
-//                                     <div className="card crypto-card">
-//                                         <div className="card-body"  >
-//                                             <Icon className="d-inline-block"  i={crypto.Code} size={45}/>
-//                                             <h5 className="d-inline"> {crypto.Name} </h5>
-//                                             {crypto.selected && <CheckCircle className="d-inline-block float-right"/>}
-//                                         </div>
-//                                     </div>
-//                                 </div>
-//                             )
-//                         })
-//                     }
-//                 </div>
-//                 {cryptoList.ttoggle && <span>Yes</span>}
-//             </div>
-//         )
-//     }
-// }
-// export default Selector;
+import "bootstrap/dist/css/bootstrap.css";
+import "./PlatformSelector.css"
+import {Search, Star} from "@material-ui/icons";
+import React from "react";
+import { SocialIcon } from 'react-social-icons';
+
+
+const platforms = [{Name:"Twitter",id:"twitter"},
+    {Name:"Reddit",id:"reddit"},
+    {Name:"Medium",id:"medium"},
+    {Name:"Discord",id:"discord"}
+];
+
+class PlatformSelector extends React.Component{
+    constructor(props) {
+        super(props);
+        this.selectPlatform = this.selectPlatform.bind(this);
+        this.state = {
+           platformsList: [...platforms.map(crypto =>{ return {...crypto,selected:false} })],
+            faveList:[]
+        };
+    }
+    selectPlatform(id){
+        let newScrapelist = this.state.faveList;
+
+        this.setState({
+            platformsList: [...this.state.platformsList.map((platform,currIndex)=>{
+                if(id==platform.id){
+                    platform.selected = !platform.selected;
+
+                    //if selected add to favourite list else remove it
+                    if(platform.selected){newScrapelist.push(platform)}
+                    else{newScrapelist = newScrapelist.filter((item) => item.id !== id);}
+                }
+                return {
+                    ...platform
+                }
+            })]
+        })
+        this.setState({faveList: [...newScrapelist.map(item =>{ return {...item} })]})
+    }
+
+    render()
+    {
+        const currState = this.state;
+
+        return(
+            <div id="quick-view" className="col-4 content-container shadow">
+                <div id="search-bar" className="input-group rounded">
+                    <input type="search" className="form-control rounded" placeholder="Search..." aria-label="Search"
+                           aria-describedby="search-addon"/>
+                    <span id="search-icon"> <Search/> </span>{/* Insert Button for search */}
+                </div>
+                <div id="table-bar">
+                    <table className="table">
+                        <thead className="thead-light">
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col"></th>
+                            <th scope="col">Name</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {
+                            currState.platformsList.map((item, index) => {
+                                return (
+                                    <tr className="crypto-row" key={item.id}>
+                                        <th scope="row">{item.selected?<Star color="primary" onClick={()=>{this.selectPlatform(item.id)}}/>:<Star color="action" onClick={()=>{this.selectPlatform(item.id)}}/>}</th>
+                                        <td><SocialIcon network={item.id} style={{height:"40px",width:"40px"}}/></td>
+                                        <td>{item.Name}</td>
+                                    </tr>
+                                );
+                            })
+                        }
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        );
+    }
+
+}
+export default PlatformSelector;

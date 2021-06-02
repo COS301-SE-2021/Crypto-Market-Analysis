3728fbb41fee831a69970af382ec346991a733f5
Lerato <u18037951@tuks.co.za>
2021-06-02 21:18:14 +0200

    updated the return crypto data

--- server/routes/twitterscapper.js
+++ server/routes/twitterscapper.js
@@ -6,6 +6,6 @@ const CoinGecko = require('coingecko-api');
 const CoinGeckoClient = new CoinGecko();
 var getData = async(name) => {
     let data = await CoinGeckoClient.coins.fetch(name, {});
-    data= data.data.tickers.filter(t => t.target == 'ZAR');
     console.log(data);
+    return data;
 };

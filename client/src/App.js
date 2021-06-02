8cb36d0a74322d5379041028eec9b05b3a46d85a
Bheki Ndhlovu <bhekindhlovu7@gmail.com>
2021-06-02 19:11:19 +0200

    adding email/auth token page to route

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import {Route, Link} from "react-router-dom";
 import login from "./components/login/login";
 import register from "./components/login/register";
 import splash from "./components/login/splash";
+import token from "./components/login/token"
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route exact path="/" component={splash}></Route>
       <Route exact path="/register" component={register}></Route>
       <Route exact path="/login" component={login}></Route>
+      <Route exact path="/token" component={token}></Route>
     </section>
   );
 }

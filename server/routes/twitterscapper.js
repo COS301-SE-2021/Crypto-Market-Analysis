806381ac9deaab9c59fbe427d6d61674e08397fc
Lerato <u18037951@tuks.co.za>
2021-06-02 21:19:35 +0200

    no longer displaying object data

--- server/routes/twitterscapper.js
+++ server/routes/twitterscapper.js
@@ -6,6 +6,6 @@ const CoinGecko = require('coingecko-api');
 const CoinGeckoClient = new CoinGecko();
 var getData = async(name) => {
     let data = await CoinGeckoClient.coins.fetch(name, {});
-    console.log(data);
+   // console.log(data);
     return data;
 };

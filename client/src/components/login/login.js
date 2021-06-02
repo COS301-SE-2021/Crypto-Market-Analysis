ea57140b71ec99945e8123faceb7c690d3d2e764
Bheki Ndhlovu <bhekindhlovu7@gmail.com>
2021-06-02 18:30:47 +0200

    http request from login to api with form parameters

--- client/src/components/login/login.js
+++ client/src/components/login/login.js
@@ -1,12 +1,12 @@
 import React from "react";
 import './css/login.css'
-
+import axios from 'axios';
 const login = () => {
     return (
         <div className="form-v5">
             <div className="page-content">
                 <div className="form-v5-content">
-                    <form className="form-detail" action="#" method="post">
+                    <form className="form-detail" onSubmit={event => login(event)}>
                         <h2>Login Form</h2>
                         <div className="form-row">
                             <label htmlFor="your-email">Email</label>
@@ -29,5 +29,13 @@ const login = () => {
         </div>
     );
 };
-
+//making a http post request to the endpoint containing form parameters
+function login(event) {
+    event.preventDefault();
+    let request = {
+        email: document.getElementById('your-email').value,
+        password: document.getElementById('password').value
+    }
+    axios.post('http://localhost:8080/user/signup', request).then(resp=>{console.log("form sent");}).catch(err=>{console.log(err);})
+}
 export default login;

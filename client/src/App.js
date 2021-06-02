f0c2ba42dec09a763ce4564d6304252ee3413926
Bheki Ndhlovu <bhekindhlovu7@gmail.com>
2021-06-02 23:55:41 +0200

    app.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import register from "./components/login/register";
 import splash from "./components/login/splash";
 import token from "./components/login/token"
 import updatePassword from "./components/login/updatePassword";
+import Home from "./components/Home/Home"
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Route exact path="/login" component={login}></Route>
       <Route exact path="/token" component={token}></Route>
       <Route exact path="/updatePassword" component={updatePassword}></Route>
+      <Route exact path="/home" component={Home}></Route>
     </section>
   );
 }

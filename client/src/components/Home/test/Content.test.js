d5a1998460999cf183ddb3fbcfd3ecbd812ff246
Bheki Ndhlovu <bhekindhlovu7@gmail.com>
2021-06-04 00:39:47 +0200

    test files

--- client/src/components/Home/test/Content.test.js
+++ client/src/components/Home/test/Content.test.js
@@ -1,17 +1,16 @@
 import React from "react";
 import ReactDom from "react-dom";
-import login from "../login";
 import {isTSAnyKeyword} from '@babel/types';
-import {cleanup, getByTestId, render} from '@testing-library/react';
-import "@testing-library/jest-dom/extend-expect";
+import {getByTestId, render,cleanup} from '@testing-library/react';
+import "@testing-library/jest-dom/extend-expect"
 import renderer from "react-test-renderer";
+import Content from "../Content/Content";
 afterEach(cleanup);
 it("renders without crashing", ()=>{
     const div =document.createElement("div");
-    ReactDom.render(<login></login>, div)
+    ReactDom.render(<Content></Content>, div)
 })
-
 it("matches snapshot",()=>{
-    const tree = renderer.create(<login></login>).toJSON();
+    const tree = renderer.create(<Content></Content>).toJSON();
     expect(tree).toMatchSnapshot();
 })
--- client/src/components/Home/test/GraphReport.test.js
+++ client/src/components/Home/test/GraphReport.test.js
@@ -1,17 +1,15 @@
 import React from "react";
 import ReactDom from "react-dom";
-import login from "../login";
 import {isTSAnyKeyword} from '@babel/types';
-import {cleanup, getByTestId, render} from '@testing-library/react';
-import "@testing-library/jest-dom/extend-expect";
+import {getByTestId, render,cleanup} from '@testing-library/react';
+import "@testing-library/jest-dom/extend-expect"
 import renderer from "react-test-renderer";
 afterEach(cleanup);
 it("renders without crashing", ()=>{
     const div =document.createElement("div");
-    ReactDom.render(<login></login>, div)
+    ReactDom.render(<GraphReport></GraphReport>, div)
 })
-
 it("matches snapshot",()=>{
-    const tree = renderer.create(<login></login>).toJSON();
+    const tree = renderer.create(<GraphReport></GraphReport>).toJSON();
     expect(tree).toMatchSnapshot();
 })

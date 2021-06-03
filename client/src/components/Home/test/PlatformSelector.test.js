b3efcbcd9d5d957493324974ad0f1106cb98fcb0
Bheki Ndhlovu <bhekindhlovu7@gmail.com>
2021-06-04 00:40:51 +0200

    test files

--- client/src/components/Home/test/PlatformSelector.test.js
+++ client/src/components/Home/test/PlatformSelector.test.js
@@ -1,17 +1,15 @@
 import React from "react";
 import ReactDom from "react-dom";
-import token from "../token";
-import {isTSAnyKeyword} from '@babel/types';
-import {cleanup, getByTestId, render} from '@testing-library/react';
+import {getByTestId, render,cleanup} from '@testing-library/react';
 import "@testing-library/jest-dom/extend-expect"
 import renderer from "react-test-renderer";
 import login from "../login";
 afterEach(cleanup);
 it("renders without crashing", ()=>{
     const div =document.createElement("div");
-    ReactDom.render(<token></token>, div)
+    ReactDom.render(<PlatformSelector></PlatformSelector>, div)
 })
 it("matches snapshot",()=>{
-    const tree = renderer.create(<token></token>).toJSON();
+    const tree = renderer.create(<PlatformSelector></PlatformSelector>).toJSON();
     expect(tree).toMatchSnapshot();
 })
--- client/src/components/login/test/splash.test.js
+++ client/src/components/login/test/splash.test.js
@@ -2,10 +2,16 @@ import React from "react";
 import ReactDom from "react-dom";
 import splash from "../splash";
 import {isTSAnyKeyword} from '@babel/types';
-import {getByTestId, render} from '@testing-library/react';
+import {cleanup, getByTestId, render} from '@testing-library/react';
 import "@testing-library/jest-dom/extend-expect"
-
+import renderer from "react-test-renderer";
+import login from "../login";
+afterEach(cleanup);
 it("renders without crashing", ()=>{
     const div =document.createElement("div");
     ReactDom.render(<splash></splash>, div)
+})
+it("matches snapshot",()=>{
+    const tree = renderer.create(<splash></splash>).toJSON();
+    expect(tree).toMatchSnapshot();
 })

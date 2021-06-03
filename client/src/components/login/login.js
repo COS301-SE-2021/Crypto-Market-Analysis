9ae5dbeb688cafa138ebf533adf9053a3c3e2d9e
Bheki Ndhlovu <bhekindhlovu7@gmail.com>
2021-06-03 20:25:34 +0200

    changing component structure

--- client/src/components/login/login.js
+++ client/src/components/login/login.js
@@ -1,45 +1,79 @@
-import React from "react";
+import React, {Component} from "react";
 import './css/login.css'
 import axios from 'axios';
-const login = () => {
-    return (
-        <div className="form-v5">
-            <div className="page-content">
-                <div className="form-v5-content">
-                    <form className="form-detail" onSubmit={event => logins(event)}>
-                        <h2>Login Form</h2>
-                        <div className="form-row">
-                            <label htmlFor="your-email">Email</label>
-                            <input type="text" name="your-email" id="your-email" className="input-text"
-                                   placeholder="Your Email" required pattern="[^@]+@[^@]+.[a-zA-Z]{2,6}"/>
+
+
+class login extends Component{
+    constructor() {
+        super();
+        this.state = {
+            email:'',
+            password:''
+        }
+        this.changePassword = this.changePassword.bind(this)
+        this.changeEmail = this.changeEmail.bind(this)
+        this.onSubmit = this.onSubmit.bind(this)
+    }
+
+    changeEmail(event){
+        this.setState({email:event.target.value})
+    }
+    changePassword(event){
+        this.setState({password:event.target.value})
+    }
+
+    onSubmit(event){
+        event.preventDefault();
+        const data = {
+            email: this.state.email,
+            password: this.state.password
+        }
+        axios
+            .post('http://localhost:8080/user/login/',data)
+            .then(() => console.log('sent'))
+            .catch(err =>{
+                console.error(err);
+                window.location = '/login'
+            });
+
+
+    }
+
+    render() {
+        return (
+            <div className="form-v5">
+                <div className="page-content">
+                    <div className="form-v5-content">
+                        <form className="form-detail" onSubmit={this.onSubmit}>
+                            <h2>Login Form</h2>
+                            <div className="form-row">
+                                <label htmlFor="your-email">Email</label>
+                                <input type="text" name="your-email" id="your-email" className="input-text"
+                                       placeholder="Your Email" required pattern="[^@]+@[^@]+.[a-zA-Z]{2,6}"
+                                       onChange={this.changeEmail}
+                                       value={this.state.email}
+                                />
                                 <i className="fas fa-envelope"></i>
-                        </div>
-                        <div className="form-row">
-                            <label htmlFor="password">Password</label>
-                            <input type="password" name="password" id="password" className="input-text"
-                                   placeholder="Your Password" required/>
+                            </div>
+                            <div className="form-row">
+                                <label htmlFor="password">Password</label>
+                                <input type="password" name="password" id="password" className="input-text"
+                                       placeholder="Your Password" required
+                                       onChange={this.changePassword}
+                                       value={this.state.password}
+                                />
                                 <i className="fas fa-lock"></i>
-                        </div>
-                        <div className="form-row-last">
-                            <input type="submit" name="register" className="register" value="Login"/>
-                        </div>
-                    </form>
+                            </div>
+                            <div className="form-row-last">
+                                <input type="submit" name="register" className="register" value="Login"/>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
-};
-//making a http post request to the endpoint containing form parameters
-function logins(event) {
-    event.preventDefault();
-    axios.post('http://localhost:8080/user/login', {
-        email: document.getElementById('your-email').value,
-        password: document.getElementById('password').value
-    })
-        .then((response) => {
-            console.log(response);
-        }, (error) => {
-            console.log(error);
-        });
+        );
+    }
 }
 export default login;
+
+

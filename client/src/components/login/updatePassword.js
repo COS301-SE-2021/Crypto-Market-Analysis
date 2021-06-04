6e69b046f7951ee4ddcc549cd52bcb88053a5bce
Bheki Ndhlovu <bhekindhlovu7@gmail.com>
2021-06-04 22:54:31 +0200

    updatePassword.js fix

--- client/src/components/login/updatePassword.js
+++ client/src/components/login/updatePassword.js
@@ -1,41 +1,87 @@
-import React from "react";
+import React,{Component} from "react";
 import './css/login.css'
 import axios from 'axios';
-const updatePassword = () => {
-    return (
-        <div className="form-v5">
-            <div className="page-content">
-                <div className="form-v5-content">
-                    <form className="form-detail" onSubmit={event => update(event)}>
-                        <h2>Update Password</h2>
-                        <div className="form-row">
-                            <label htmlFor="your-email">New Password</label>
-                            <input type="text" name="your-email" id="your-email" className="input-text"
-                                   placeholder="Your Email" required pattern="[^@]+@[^@]+.[a-zA-Z]{2,6}"/>
-                            <i className="fas fa-envelope"></i>
-                        </div>
-                        <div className="form-row">
-                            <label htmlFor="your-token">Token</label>
-                            <input type="your-token" name="your-token" id="your-token" className="input-text"
-                                   placeholder="Your Token" required/>
-                            <i className="fas fa-lock"></i>
-                        </div>
-                        <div className="form-row-last">
-                            <input type="submit" name="token" className="register" value="Submit"/>
-                        </div>
-                    </form>
+
+class updatePassword extends Component{
+    constructor()
+    {
+        super();
+        this.state = {
+            token:'',
+            password:''
+        }
+        this.changeToken = this.changeToken.bind(this)
+        this.changePassword = this.changePassword.bind(this)
+        this.onSubmit = this.onSubmit.bind(this)
+    }
+    changeToken(event){
+        this.setState({token:event.target.value})
+    }
+    changePassword(event){
+        this.setState({password:event.target.value})
+    }
+
+    onSubmit(event)
+    {
+        event.preventDefault();
+        const updatePassword = {
+            token: this.state.token,
+            email: this.state.password
+        }
+
+        axios
+            .post('http://localhost:8080/user/updatePassword/',updatePassword)
+            .then(() =>{window.location = '/home';} )
+            .catch(err =>{
+                console.error(err);
+
+            });
+    }
+    render()
+    {
+        return (
+            <div className="form-v5">
+                <div className="page-content">
+                    <div className="form-v5-content">
+                        <form className="form-detail" onSubmit={this.onSubmit}>
+                            <h2>Update Password</h2>
+                            <div className="form-row">
+                                <label htmlFor="your-email">New Password</label>
+                                <input type="text" name="your-email" id="your-email" className="input-text"
+                                       placeholder="Your Email" required pattern="[^@]+@[^@]+.[a-zA-Z]{2,6}"
+                                       onChange={this.changePassword}
+                                       value={this.state.password}
+                                />
+                                <i className="fas fa-envelope"></i>
+                            </div>
+                            <div className="form-row">
+                                <label htmlFor="your-token">Token</label>
+                                <input type="your-token" name="your-token" id="your-token" className="input-text"
+                                       placeholder="Your Token" required
+                                       onChange={this.changeToken}
+                                       value={this.state.token}
+                                />
+                                <i className="fas fa-lock"></i>
+                            </div>
+                            <div className="form-row-last">
+                                <input type="submit" name="token" className="register" value="Submit"/>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
-};
-//making a http post request to the endpoint containing form parameters
-function update(event) {
-    event.preventDefault();
-    let request = {
-        token: document.getElementById('your-token').value,
-        password: document.getElementById('your-password').value
-    };
-    axios.post('http://localhost:8080/user/updatePassword', request).then(resp=>{console.log("form sent");}).catch(err=>{console.log(err);})
+        );
+    }
 }
 export default updatePassword;
+
+
+
+
+
+
+
+
+
+
+

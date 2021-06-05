9232625f873a2641781f2c135e1fc94b20b0cccc
Zee864 <82458469+Zee864@users.noreply.github.com>
2021-06-05 12:28:38 +0200

    Revert "Merge branch 'user-develop' of https://github.com/COS301-SE-2021/Crypto-Market-Analysis"
    This reverts commit 152ee59898498b6e91965d60a3445d7e1e085cc1, reversing
    changes made to 0f1b49e6a171cb062bbb92b8e1873180f5beb401.

--- client/src/components/login/register.js
+++ client/src/components/login/register.js
@@ -39,7 +39,7 @@ class register extends Component{
         axios
             .post('http://localhost:8080/user/signup/',registered)
             .then(() =>{console.log('sent');
-                window.location = '/token';}
+                window.location = '/home';}
             )
             .catch(err =>{
                 console.error(err);

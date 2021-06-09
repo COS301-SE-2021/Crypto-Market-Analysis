c130951ddc1d51f87c6b6b5260f5b3bc03e38d49
Bheki Ndhlovu <bhekindhlovu7@gmail.com>
2021-06-09 18:56:53 +0200

    firebase dependency

--- server/crypto_user_functions.js
+++ server/crypto_user_functions.js
@@ -2,6 +2,8 @@ const Twitter = require('twitter-v2');
 const snoowrap = require('snoowrap');
 const Sentiment = require('sentiment');
 const vader = require('vader-sentiment');
+const admin = require('firebase-admin');
+const serviceAccount = require('codex-crypto-market-analysis-98cca9c9a2cc.json');
 
 //initialize
 const consumer_key = 'GGXUovWNfvGvagGakjfTzDfe1';
@@ -34,12 +36,18 @@ async function scrapeSubreddit() {
     const subreddit4 = await r.getSubreddit('SatoshiStreetBets');
     const subreddit5 = await r.getSubreddit('CryptoCurrencies');
 
+    let Cryptomarkets = [];
+    let CryptoCurrencyTrading = [];
+    let Crypto_Currency_News = [];
+    let SatoshiStreetBets = [];
+    let CryptoCurrencies = [];
 
     const topPosts1 = await subreddit1.getTop({ limit: 100});
     const topPosts2 = await subreddit2.getTop({ limit: 100});
     const topPosts3 = await subreddit3.getTop({ limit: 100});
     const topPosts4 = await subreddit4.getTop({ limit: 100});
     const topPosts5 = await subreddit5.getTop({ limit: 100});
+
 
     let data = [];
     let dataset = [];
@@ -49,27 +57,68 @@ async function scrapeSubreddit() {
 
     topPosts1.forEach((post) => {
         data.push(post.title);
+        Cryptomarkets.push(post.title);
     });
     topPosts2.forEach((post) => {
         data.push(post.title);
+        CryptoCurrencyTrading.push(post.title);
     });
     topPosts3.forEach((post) => {
         data.push(post.title);
+        Crypto_Currency_News.push(post.title);
     });
     topPosts4.forEach((post) => {
         data.push(post.title);
+        SatoshiStreetBets.push(post.title);
     });
     topPosts5.forEach((post) => {
         data.push(post.title);
+        CryptoCurrencies.push(post.title);
     });
+
+    for (var i = 0; i < data.length; i++)
+    {
+        for(var x = 0; x<tags.length;x++)
+        {
+            if(data[i].includes(tags[x]))
+            {
+                //console.log(data[i]);
+                dataset.push(data[i]);
+            }
+        }
+    }
+    uniquedataset = dataset.filter((a, b) => dataset.indexOf(a) === b);
+    //console.log(uniquedataset);
+
+
+    var sentiment = new Sentiment();
+    uniquedataset.forEach(function(s){
+        console.log(sentiment.analyze(s));
+    })
+
+    uniquedataset.forEach(function(s){
+        console.log( vader.SentimentIntensityAnalyzer.polarity_scores(s));
+    })
+
+    //firebase saving of scrapped data
+
+    admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount)
+    });
+
+    const db = admin.firestore();
+
+    const docRef = db.collection('sub_reddit_data').doc('leO350yIvsRsnjhtLizZ');
+
+    await docRef.set({
+        CryptoCurrencies: CryptoCurrencies,
+        CryptoCurrencyTrading: CryptoCurrencyTrading,
+        Crypto_Currency_News: Crypto_Currency_News,
+        Cryptomarkets: Cryptomarkets,
+        SatoshiStreetBets: SatoshiStreetBets,
+    });
+
 }
-
-
-
-
-
-
-
 
 const getTweets = async () => {
     /*const { data } = await client.get('statuses/user_timeline', { screen_name: 'elonmusk' });
@@ -81,6 +130,7 @@ const getTweets = async () => {
 
 
 getTweets().then();
+scrapeSubreddit();
 
 
 

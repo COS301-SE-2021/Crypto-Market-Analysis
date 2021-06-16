896b322ebd3a25093f9edc9ecf37248df6530a29
Zee864 <82458469+Zee864@users.noreply.github.com>
2021-06-16 15:06:28 +0200

    Deleted Client folder

--- client/src/components/Home/test/GraphReport.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDom from "react-dom";
-import {isTSAnyKeyword} from '@babel/types';
-import {getByTestId, render,cleanup} from '@testing-library/react';
-import "@testing-library/jest-dom/extend-expect"
-import renderer from "react-test-renderer";
-afterEach(cleanup);
-
-

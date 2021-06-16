9eb04e17fc053884388dd6dbb55c77ce330fa93a
Lerato <u18037951@tuks.co.za>
2021-06-17 00:18:48 +0200

    fixed route position

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
           <AuthProvider>
            <Switch>
                  <section className="header">
-                     <Route path="/" component={splash}></Route>
+                     <Route exact path="/" component={splash}></Route>
                      <Route exact path="/register" component={Register}></Route>
                      <PrivateRoute exact path="/home" component={Home}></PrivateRoute>
                      <Route exact path="/login" component={login}></Route>

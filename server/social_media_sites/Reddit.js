fddc3f339491fb5ded923d33549988787676760e
Bheki Ndhlovu <bhekindhlovu7@gmail.com>
2021-06-17 23:21:06 +0200

    test

--- server/social_media_sites/Reddit.js
+++ server/social_media_sites/Reddit.js
@@ -26,6 +26,16 @@ class Reddit {
         this.#firestore_db.save('reddit_data',Subreddit,'posts',empty);
         this.#firestore_db.save('reddit_data',Subreddit,'posts',Data);
     }
+    scrapeSubreddit2 = async (Subreddit) => {
+        const subreddit1 = await r.getSubreddit(Subreddit);
+        let Data = [];
+        const topPosts1 = await subreddit1.getTop({limit: 100});
+        topPosts1.forEach((post) => {
+            Data.push(post.title);
+        });
+        this.#firestore_db.save('Reddit',Subreddit,'subreddits',empty);
+        this.#firestore_db.save('Reddit',Subreddit,'subreddits',Data);
+    }
 }
 let reddits = new Reddit();
 reddits.scrapeSubreddit("CryptoCurrencies").then();
@@ -33,3 +43,5 @@ reddits.scrapeSubreddit("SatoshiStreetBets").then();
 reddits.scrapeSubreddit("Crypto_Currency_News").then();
 reddits.scrapeSubreddit("CryptoCurrencyTrading").then();
 reddits.scrapeSubreddit("Cryptomarkets").then();
+reddits.scrapeSubreddit2("Bitcoin").then();
+reddits.scrapeSubreddit2("Ethereum").then();

b9b8795752d8d56663048ae9ba7f2b5e6fd86ab3
Lerato <u18037951@tuks.co.za>
2021-06-19 22:56:03 +0200

    added analysis test

--- server/_tests_/user.spec.js
+++ server/_tests_/user.spec.js
@@ -1,5 +1,5 @@
 const conv = require('../routes/analysisFunction');
-describe("filtering the post", () => {
+describe("Testing the conversion function", () => {
     test("it should remove stop words", () => {
          const expected = 'this is the test'
         conv.convertion('this is the test').then(data=>{
@@ -30,3 +30,90 @@ describe("filtering the post", () => {
 
     });
 });
+describe("Testing the split function", () => {
+    test("Testing with incorrect expected data", () => {
+        const expected = "";
+        conv.splits("i want to split these words").then(data=> {
+            expect(data).not.toEqual(expected);
+        }).catch(err=>{
+            console.log(err);
+        })
+
+    });
+
+    test("Testing with correct expected data", () => {
+        const expected = [ 'i', 'want', 'to', 'split', 'these', 'words' ];
+        conv.splits("i want to split these words").then(data=> {
+            expect(data).toEqual(expected);
+        }).catch(err=>{
+            console.log(err);
+        })
+
+    });
+    test("Testing null value", () => {
+        const expected = [ 'i', 'want', 'to', 'split', 'these', 'words' ];
+         conv.splits(null).then(data=> {
+            expect(data).not.toEqual(["null"]);
+        }).catch(err=>{
+
+        })
+    });
+})
+describe("Testing the spellingc function", () => {
+    test("incorrect data", () => {
+        const expected = [""];
+        conv.spellingc(['90d282dd2d']).then(data=> {
+            expect(data).not.toEqual(expected);
+        }).catch(err=>{
+            console.log(err);
+        })
+
+    });
+
+    test("Testing with correct expected data", () => {
+        const expected = ['happiness'];
+        conv.spellingc(['hapeness']).then(data=> {
+            expect(data).toEqual(expected);
+        }).catch(err=>{
+            console.log(err);
+        })
+
+    });
+    test("Testing null value", () => {
+        const expected = [ 'i', 'waht', 'to', 'split', 'these', 'words' ];
+        conv.spellingc(null).then(data=> {
+            expect(data).not.toEqual(null);
+        }).catch(err=>{
+
+        })
+    });
+})
+describe("Testing the analysewords function", () => {
+    test("incorrect data", () => {
+        const expected = [""];
+        conv.analysewords(['90d282dd2d']).then(data=> {
+            expect(data).not.toEqual(expected);
+        }).catch(err=>{
+            console.log(err);
+        })
+
+    });
+
+    test("Testing with correct expected data", () => {
+        const expected = ['happiness'];
+        conv.analysewords(['hapeness']).then(data=> {
+            expect(data).toEqual(0);
+        }).catch(err=>{
+            console.log(err);
+        })
+
+    });
+    test("Testing null value", () => {
+        const expected = [ 'i', 'waht', 'to', 'split', 'these', 'words' ];
+        conv.analysewords(null).then(data=> {
+            expect(data).not.toEqual(null);
+        }).catch(err=>{
+
+        })
+    });
+})

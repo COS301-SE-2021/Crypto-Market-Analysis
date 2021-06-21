f0964d470bbfc7dd76e522553dc59939a11b03be
Lerato <u18037951@tuks.co.za>
2021-06-21 02:16:02 +0200

    Mock firestore db test

--- server/_tests_/twitter.spec.js
+++ server/_tests_/twitter.spec.js
@@ -1,19 +1,78 @@
 const Twitter = require('../social_media_sites/Twitter');
 const twitter = new Twitter();
+const { FakeFirestore } = require('firestore-jest-mock');
+const { mockCollection, mockDoc } = require('firestore-jest-mock/mocks/firestore');
 
-describe('getUserTimeline', () => {
-    test("testing negative inputs", () => {
-        let input = [];
-        let output = -2;
-        expect(twitter.getUserTimeline("String")).toEqual(output);
-        expect(twitter.getUserTimeline([1,2,3,4])).toEqual(output);
-        input.push("abcdefghifjklmno");
-        twitter.getUserTimeline(input);
+describe('Database', () => {
+    beforeEach(() => {
+        jest.resetModules();
+        jest.clearAllMocks();
     });
 
-    test("testing boundaries", () => {
-        let input = [];
-        const output = -1;
-        expect(twitter.getUserTimeline(input)).toEqual(output);
+    const db = (simulateQueryFilters = false) =>
+        new FakeFirestore(
+            {
+                characters: [
+                    {
+                        id: 'mojohnnylerato@gmail.com',
+                          Twitter: {
+                            Bitcoin: [
+                                { Average: 12, Min: 0, Max: 4, AnalysisArr:[1,2,3,4,5] },
+                                { post: 'this is a tweet about bitcoin' },
+                            ],
+                        },
+                    },
+                ],
+            },
+            { simulateQueryFilters },
+        );
+    test('querying the database test positive case', async () => {
+        expect.assertions(4);
+        const data = await db()
+            .collection('Twitter')
+            .doc('Bitcoin')
+            .get();
+        expect(mockCollection).toHaveBeenCalledWith('Twitter');
+        expect(mockDoc).toHaveBeenCalledWith('Bitcoin');
+        expect(data.id).toBe('Bitcoin');
+        expect(data.exists).toBe(false);
     });
-});
+    test('querying the database test negative case', async () => {
+        expect.assertions(4);
+        const data = await db()
+            .collection('Twitter')
+            .doc('Bitcoin')
+            .get();
+        expect(mockCollection).not.toHaveBeenCalledWith('Reddit');
+        expect(mockDoc).not.toHaveBeenCalledWith('Ethereum');
+        expect(data.id).not.toBe('Ethereum');
+        expect(data.exists).not.toBe(true);
+    });
+    test('saving to the database test positive case', async () => {
+        const mock= db();
+        await mock
+            .collection('Twitter')
+            .doc('Bitcoin')
+            .set({
+                post: 'this is the data from twitter',
+            });
+        expect(mockCollection).toHaveBeenCalledWith('Twitter');
+        expect(mockDoc).toHaveBeenCalledWith('Bitcoin');
+        const doc = await mock.doc('Twitter/Bitcoin').get();
+        expect(doc.id).toBe('Bitcoin');
+    });
+    test('saving to the database test negative case', async () => {
+        const mock= db();
+        await mock
+            .collection('Reddit')
+            .doc('Ethereum')
+            .set({
+                post: 'this is the data from twitter',
+            });
+        expect(mockCollection).not.toHaveBeenCalledWith('Twitter');
+        expect(mockDoc).not.toHaveBeenCalledWith('Bitcoin');
+        const doc = await mock.doc('Reddit/Ethereum').get();
+        expect(doc.id).not.toBe('Bitcoin');
+    });
+
+})

b7043b969b90ed15118f320c4c84dc026777f6aa
Lerato <u18037951@tuks.co.za>
2021-06-25 02:12:46 +0200

    updated follow crypto

--- server/routes/user.js
+++ server/routes/user.js
@@ -77,58 +77,11 @@ router.post("/followCrypto", async (request,response)=>{
     if(request.body.email === null || request.body.symbol === null || request.body.crypto_name === null)
         return response.status(401).json({status: `Bad Request`, error: `Malformed request. Please check your parameters`});
     else{
-        const email = request.body.email;
-        let crypto = [];
-        let crypto_name = [];
-        let data = {};
-        let found = false;
-        let docRef = null;
-        try{
-            docRef = db.collection(`Users`).doc(email)
-        }
-        catch (err) {
-            return response.status( 500).json({status: `Internal Server Error`, error: `The document could not be retrieved: ${err}`});
-        }
-
-        try{
-            await db.collection(`Users`).get().then((snapshot) =>{
-                for (const doc of snapshot.docs) {
-                    if(doc.id === email){
-                        found = true;
-                        if(doc.data().crypto)
-                            crypto = doc.data().crypto;
-                        else
-                            crypto = [];
-                        if(doc.data().crypto_name)
-                            crypto_name = doc.data().crypto_name;
-                        else
-                            crypto_name = [];
-                        break;
-                    }
-                }
-            });
-
-            if(found === false)
-                return response.status(403).json({status: `Not authorized`, error: `The user does not exist`})
-
-            if(crypto.find(element => element === request.body.crypto) === undefined && crypto_name.find(element => element === request.body.crypto_name) === undefined){
-                crypto.push(request.body.symbol);
-                crypto_name.push(request.body.crypto_name);
-            }
-            else
-                return response.status(202).json({status: `Accepted`, message: `The cryptocurrency already exists`});
-            data = {[`crypto`]: crypto,[`crypto_name`]: crypto_name}
-            try{
-                await docRef.set(data, {merge:true});
-            }
-            catch (err){
-                return response.status( 500).json({status: `Internal Server Error`, error: `The crypto could not be added to the database: ${err}`});
-            }
-            return response.status(200).json({status: `Ok`, message: `The crypto been successfully added`});
-        }
-        catch(err){
+        await userFunctions.followCrypto(request.body.email,request.body.symbol,request.body.crypto_name).then(data=>{
+            return response.status(200).json(data);
+        }).catch(err=>{
             return response.status(500).json({status:`Internal server error`, error: err})
-        }
+        })
     }
 });
 

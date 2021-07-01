de2fa4cf70f47773748c87743df803b8e4f2d13b
Bheki Ndhlovu <bhekindhlovu7@gmail.com>
2021-07-01 14:30:57 +0200

    4chan

--- server/social_media_sites/4chan.js
+++ server/social_media_sites/4chan.js
@@ -1,5 +1,8 @@
 const Nightmare = require('nightmare');
 const cheerio = require('cheerio');
 const fs = require('fs');
-
 const board = "biz";
+
+const url = (board) => {
+    return  'http://boards.4chan.org/' + board + '/catalog';
+}

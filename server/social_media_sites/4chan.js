22c4020eab9f54b5e0834aa19138bf4b056aae49
Bheki Ndhlovu <bhekindhlovu7@gmail.com>
2021-07-01 14:31:27 +0200

    4chan

--- server/social_media_sites/4chan.js
+++ server/social_media_sites/4chan.js
@@ -6,3 +6,19 @@ const board = "biz";
 const url = (board) => {
     return  'http://boards.4chan.org/' + board + '/catalog';
 }
+
+const crawlCatalogue = async() => {
+    console.log("Starting crawl process for /" + board + "/");
+    const nightmare = new Nightmare({ show: false });
+    try {
+        let threadArray = [];
+        let threads = await nightmare.goto(url(board))
+            .wait('#threads .thread')
+            .evaluate(() => {
+                let elements = Array.from(document.getElementsByClassName('thread'));
+                return elements.map(elem => {
+                    return {
+                        content: elem.innerHTML
+                    }
+                });
+            })

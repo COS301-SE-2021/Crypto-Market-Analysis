d71642764b46cdb0723f0ccd8ba2e92d96a27358
Bheki Ndhlovu <bhekindhlovu7@gmail.com>
2021-07-01 14:32:01 +0200

    4chan

--- server/social_media_sites/4chan.js
+++ server/social_media_sites/4chan.js
@@ -22,3 +22,22 @@ const crawlCatalogue = async() => {
                     }
                 });
             })
+            .end()
+            .then(threads => {
+                console.log('Found ' + threads.length + ' threads. Mapping to objects...');
+                let formatted = threads.map(item => {
+                    let $ = cheerio.load(item.content);
+                    data =
+                        {
+                            //id: $('.meta a').attr('data-post-menu'),
+                            //    link: $('a').attr('href'),
+                            //    img: $('img').attr('src'),
+                            //     replies: $('.meta > b:nth-child(1)').text(),
+                            //    imgReplies: $('.meta > b:nth-child(2)').text(),
+                            //     title: $('.teaser > b').first().text(),
+                            op: $('.teaser').text()
+                            // title: $('.postMessage').text()
+                        }
+                    return data;
+
+                });

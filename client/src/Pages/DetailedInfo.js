36bfb5acbb95af93cf2aff3dd8d227fd3fdd3e6f
kg <kgothalang451@gmail.com>
2021-07-05 09:53:34 +0200

    added detailed page

--- client/src/Pages/DetailedInfo.js
+++ client/src/Pages/DetailedInfo.js
@@ -4,9 +4,22 @@ import "bootstrap/dist/css/bootstrap.css";
 import CardStats from "../components/Home/Cards/CardStats" ;
 import CardTweets from "../components/Home/Cards/CardTweets/CardTweets" ;
 import SentimentSpeedometer from "../components/Home/GraphReport/AnalysisGraph"
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Markup } from 'react-render-markup';
 
 export default function DetailedInfo() {
+
+    let [coin, setCoin] = useState([]);
+
+    useEffect(async () => {
+        console.log("USEFFECT")
+        axios.get('https://api.coingecko.com/api/v3/coins/bitcoin')
+        .then(async(response) => {
+            setCoin(response.data)
+        })
+        .catch(err => {console.error(err);})
+    },[])
 const responsive = {
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
@@ -24,44 +37,106 @@ const responsive = {
       slidesToSlide: 1 // optional, default to 1.
     }
   };
-
+let v = "Miners today are mining Bitcoin using ASIC chip dedicated to only mining Bitcoin, and the hash rate has shot up to peta hashes.\r\n\r\nBeing the first successful online cryptography currency, Bitcoin has inspired other alternative currencies such as <a href=\"https://www.coingecko.com/en/coins/litecoin\">Litecoin</a>, <a href=\"https://www.coingecko.com/en/coins/peercoin\">Peercoin</a>, <a href=\"https://www.coingecko.com/en/coins/primecoin\">Primecoin</a>, and so on.\r\n\r\nThe cryptocurrency then took off with the innovation of the turing-complete smart contract by <a href=\"https://www.coingecko.com/en/coins/ethereum\">Ethereum</a> which led to the development of other amazing projects such as <a href=\"https://www.coingecko.com/en/coins/eos\">EOS</a>, <a href=\"https://www.coingecko.com/en/coins/tron\">Tron</a>, and even crypto-collectibles such as <a href=\"https://www.coingecko.com/buzz/ethereum-still-king-dapps-cryptokitties-need-1-billion-on-eos\">CryptoKitties</a>."
   return(
       <>
-      
+      {console.log(coin)}
         <div className="container">
-            {/* <div style={{borderTop:"1px solid grey",borderBottom:"1px solid grey"}}><h4 className="display-4">Cryptocurrency</h4></div> */}
-            <div style={{width:"70%",margin:"auto",height:"190px"}}><CardStats/></div>
+            <hr />
+            <ul className="nav nav-pills nav-fill">
+                <li className="nav-item">
+                    <a className="nav-link"  href="#">Overview</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#tweets">Tweets</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">Reddit</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                </li>
+            </ul>
+            <hr />
+        </div>
+        <div className="container mt-16 mb-12">
+            <div className="row">
+                <div className="col-4">
+                    <img src={coin.image.large}/>
+                </div>
+                <div className="col-8">
+                    <p className="text-sm">  <Markup markup={coin.description.en} /></p>
+                </div>
+            </div>
         </div>
 
-        <div className="container">
-       
-            <div style={{borderTop:"1px solid grey",borderBottom:"1px solid grey",}}><h4 className="display-4">Sentiment Analyis</h4></div>
+        <div className="container mb-3" style={{margin:"auto"}}>
+            <div className="row">
+                <div className="col-12">
+                    <div className="d-inline"><span className="badge badge-primary rounded-circle p-4"><i class="fas fa-hashtag fa-3x"></i><h1 className="d-inline ml-2">{coin.market_cap_rank}</h1></span></div>
+                    <div className="d-inline float-right mt-4 uppercase font-bold p-2 px-0" ><a style={{color:"black",textDecoration:"none"}} href={coin.links.homepage}> <i class="fas fa-link"></i> Visit {coin.name} </a></div>
+                </div>
+            </div>
+        </div>
+        <div className="container" style={{margin:"auto"}}>
+             <div className="row" style={{textAlign:"center"}}>
+
+                <div className="col-4">
+                <div className="uppercase font-bold p-2 px-0" style={{color:"#58667e"}}>Current price</div> 
+                    <h2 className="font-bold">R {coin.market_data.current_price.zar.toLocaleString()}</h2>
+                    <hr/>
+                </div>
+                <div className="col-4">
+                    <div className="uppercase font-bold p-2 px-0" style={{color:"#58667e"}}>Market cap</div> 
+                    <h2 className="font-bold">R {coin.market_data.market_cap.zar.toLocaleString()}</h2>
+                    <hr/>
+                </div>
+                <div className="col-4">
+                    <div className="uppercase font-bold p-2 px-0" style={{color:"#58667e"}}>Total volume</div> 
+                    <h2 className="font-bold">R {coin.market_data.total_volume.zar.toLocaleString()}</h2>
+                    <hr/>
+                </div>
+            </div>
             
-        <Carousel
-        swipeable={true}
-        draggable={true}
-        showDots={true}
-        responsive={responsive}
-        ssr={true} // means to render carousel on server-side.
-        infinite={true}
-        autoPlaySpeed={1000}
-        keyBoardControl={true}
-        customTransition="all .5"
-        transitionDuration={500}
-        containerClass=""
-        removeArrowOnDeviceType={["tablet", "mobile"]}
-        dotListClass="custom-dot-list-style"
-        itemClass=""
-    >
-         
-          <div style={{margin:"10px",height:"190px"}}><SentimentSpeedometer/></div>
-          <div style={{margin:"10px",height:"190px"}}><SentimentSpeedometer/></div>
-          <div style={{margin:"10px",height:"190px"}}><SentimentSpeedometer/></div>
-          <div style={{margin:"10px",height:"190px"}}><SentimentSpeedometer/></div>
-    </Carousel>
-    </div>
-    {/* <hr /> */}
-    <div className="container mt-16">
+        </div>
+       
+  
+        <div className="container mt-16">
+            <div className="row"> 
+                <div className="col-8">
+                </div>
+                <div className="col-4">
+                    <table class="table">
+                        <tbody>
+                            <tr>
+                                <td>Price change in 1 hour</td>
+                                <td >{coin.market_data.price_change_percentage_1h_in_currency.zar > 0 ? <span className="badge badge-success ml-2">{coin.market_data.price_change_percentage_1h_in_currency.zar}</span> : <span className="badge badge-danger ml-2">{coin.market_data.price_change_percentage_1h_in_currency.zar}</span>} </td>
+                            </tr>
+                            <tr>
+                                <td>Price change in 24 hours</td>
+                                <td>{coin.market_data.price_change_percentage_24h_in_currency.zar > 0 ? <span className="badge badge-success ml-2">{coin.market_data.price_change_percentage_24h_in_currency.zar}</span> : <span className="badge badge-danger ml-2">{coin.market_data.price_change_percentage_24h_in_currency.zar}</span>}</td>
+                            </tr>
+                            <tr>
+                                <td>Price change in 7 days</td>
+                                <td>{coin.market_data.price_change_percentage_7d_in_currency.zar > 0 ? <span className="badge badge-success ml-2">{coin.market_data.price_change_percentage_7d_in_currency.zar}</span> : <span className="badge badge-danger ml-2">{coin.market_data.price_change_percentage_7d_in_currency.zar}</span>}</td>
+                            </tr>
+                            <tr>
+                                <td>Price change in 14 days</td>
+                                <td>{coin.market_data.price_change_percentage_14d_in_currency.zar > 0 ? <span className="badge badge-success ml-2">{coin.market_data.price_change_percentage_14d_in_currency.zar}</span> : <span className="badge badge-danger ml-2">{coin.market_data.price_change_percentage_14d_in_currency.zar}</span>}</td>
+                            </tr>
+                            <tr>
+                                <td>Price change in 30 days</td>
+                                <td>{coin.market_data.price_change_percentage_30d_in_currency.zar > 0 ? <span className="badge badge-success ml-2">{coin.market_data.price_change_percentage_30d_in_currency.zar}</span> : <span className="badge badge-danger ml-2">{coin.market_data.price_change_percentage_30d_in_currency.zar}</span>}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+        <div className="container">
+        <div className=" text-sm p-2 px-0" ><span className="uppercase font-bold">Last updated at : </span> {coin.market_data.last_updated}</div>
+        </div>
+    <div id="tweets" className="container mt-16">
             
             <div style={{borderTop:"1px solid grey",borderBottom:"1px solid grey"}}><h4 className="display-4">Tweets</h4></div>
             
--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -10,15 +10,14 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 import "../../assets/styles/tailwind.css";
 
 function Home(){
-    console.log("Triggered")
     return (
         <>
         <Sidebar />
         <div className="md:ml-64 bg-blueGray-100 pt-4">
         {/* <AdminNavbar /> */}
         {/* Header */}
-        <HeaderStats />
-        {/* <DetailedInfo /> */}
+        {/* <HeaderStats /> */}
+        <DetailedInfo />
         <div className="px-4 md:px-10 mx-auto w-full -m-24">
             {/* <Switch>
             <Route path="/admin/dashboard" exact component={Dashboard} />

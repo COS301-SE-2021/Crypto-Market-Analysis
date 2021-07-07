ac2dfcf25ea70b94ee40cc99283fe3049eb0f35c
MelussiMkhonta <u14079412@tuks.co.za>
2021-07-07 03:05:30 +0200

    edited Profile page

--- client/src/Pages/Profile/Profile.js
+++ client/src/Pages/Profile/Profile.js
@@ -1,10 +1,21 @@
 import React from 'react'
-import {Button} from 'react-bootstrap'
+//import {Button} from 'react-bootstrap'
 import Tabs from 'react-bootstrap/Tabs'
 import Tab from 'react-bootstrap/Tab'
 import { Link } from "react-router-dom";
-//import { IconName } from "@react-icons/all-files/fa/FaUserEdits"
-//import styled from 'styled-components';
+import { IconName } from "@react-icons/all-files/fa/FaUserEdits"
+import styled from 'styled-components';
+
+const Button = styled.button`
+display: block;
+text-align: center;
+background-color: #d3d3d3;
+color:black;
+padding: 5px 15px;
+border-radius: 5px;
+outline: 5px;
+width: 150%;
+`
 
 export default function Profile()
 {
@@ -34,39 +45,49 @@ export default function Profile()
                     src="https://static.vecteezy.com/system/resources/previews/002/318/271/non_2x/user-profile-icon-free-vector.jpg"
                     />
 
-                    <Link
-                        className={
-                            "text-xs lowercase py-3 font-bold block " +
-                            (window.location.href.indexOf("/updateProfile") !== -1
-                                ? "text-lightBlue-500 hover:text-lightBlue-600"
-                                : "text-blueGray-700 hover:text-blueGray-500")
-                        }
-                        to="/updateProfile"
-
-                        style={{position:"centre"}}
-                    >
-
-                        <i
-                            className={
-                                "fas fa-User-Edit-alt mr-2 text-sm " +
-                                (window.location.href.indexOf("/updateProfile") !== -1
-                                    ? "opacity-75"
-                                    : "text-blueGray-300")
-                            }
-                        ></i>{" "}
-                            Edit Profile
-                    </Link>
                 </div>
+
                 <div>
                     <div style={{display:"flex",justifyContent:"space-between", width: "108%"}}>
                         <h4>{cryptoReq.email}</h4>
 
                     </div>
 
+
                     <div style={{display:"flex",justifyContent:"space-between", width: "108%"}}>
                         <h6>Follows 2 cryptos</h6>
                         <h6>Follows 3 social media sites</h6>
                     </div>
+
+                    <div style={{display:"flex",justifyContent:"space-between", width: "108%",  position: "relative", bottom: "-50px", right: "-100px"}}>
+                        <Link
+                            className={
+                                "text-xs lowercase py-3 font-bold block " +
+                                (window.location.href.indexOf("/updateProfile") !== -1
+                                    ? "text-lightBlue-500 hover:text-lightBlue-600"
+                                    : "text-blueGray-700 hover:text-blueGray-500")
+                            }
+                            to="/updateProfile"
+
+                            style={{position:"centre", display:"flex", font:"verdana"}}
+                        >
+
+                            <i
+                                className={
+                                    "fas fa-User-Edit mr-2 text-sm " +
+                                    (window.location.href.indexOf("/updateProfile") !== -1
+                                        ? "opacity-75"
+                                        : "text-blueGray-300")
+                                }
+                            ></i>{" "}
+                            <Button>
+                                Edit Profile
+                            </Button>
+                        </Link>
+                    </div>
+
+
+
 
                 </div>
             </div>
--- client/src/components/Sidebar/Sidebar.js
+++ client/src/components/Sidebar/Sidebar.js
@@ -22,7 +22,7 @@ export default function Sidebar() {
           </button>
           {/* Brand */}
           <Link
-            className="md:block text-left md:pb-2 text-blueGray-600 mr-0 inline-block whitespace-nowrap text-sm uppercase font-bold p-4 px-0"
+            className="md:block text-left md:pb-2 text-blueGray-600 mr-0 inline-block whitespace-nowrap text-sm uppercase font-bold  p-4 px-0"
             to="/"
           >
             Cryptosis

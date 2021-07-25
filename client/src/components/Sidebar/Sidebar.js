31dc5e7dc7ce181983c5b20fa15f92d03197658e
kg <kgothalang451@gmail.com>
2021-07-26 01:31:41 +0200

    update

--- client/src/components/Sidebar/Sidebar.js
+++ client/src/components/Sidebar/Sidebar.js
@@ -62,17 +62,19 @@ export default function Sidebar() {
   return (
   
     <>
-      <Modal show={show}>
-        <Modal.Body style={{textAlign:"center"}}>
-          <>Oops, you're not logged in</>
+      <Modal show={show} style={{textAlign:"center"}}>
+        <Modal.Body >
+          <h4>Oops</h4>
+          <p>Looks like you're not logged in. Please log in to access to more features</p>
         </Modal.Body>
-        <Modal.Footer>
+        <Modal.Footer className="justify-center" >
           <Button onClick={onCancel} className="btn btn-danger">Cancel</Button>
-          <Button onClick={OnContinue} className="btn btn-primary">Login</Button>
+          <Button onClick={OnContinue} className="btn btn-success">Login</Button>
         </Modal.Footer>
       </Modal>
       <nav className="md:left-0 md:block md:fixed md:top-0 md:bottom-0 md:overflow-y-auto md:flex-row md:flex-nowrap md:overflow-hidden shadow-xl bg-white flex flex-wrap items-center justify-between relative md:w-64 z-10 py-4 px-6"
       >
+    
         <div className="md:flex-col md:items-stretch md:min-h-full md:flex-nowrap px-0 flex flex-wrap items-center justify-between w-full mx-auto">
           {/* Toggler */}
           <button

c88fa3de657562ec3e84d2edd60ece24ac451793
Bheki Ndhlovu <bhekindhlovu7@gmail.com>
2021-07-26 22:15:23 +0200

    hydra

--- server/social_media_sites/chan.js
+++ server/social_media_sites/chan.js
@@ -4,6 +4,8 @@ const fs = require('fs');
 const board = "biz";
 const empty = [];
 const Database = require('../database/Database');
+var Filter = require('bad-words'),
+filter = new Filter();
 
 const url = (board) => {
     return  'http://boards.4chan.org/' + board + '/catalog';
@@ -34,14 +36,15 @@ class chan {
                         let $ = cheerio.load(item.content);
                         let data =
                             {
-                                //id: $('.meta a').attr('data-post-menu'),
+                                //postid: $('.meta a').attr('data-post-menu'),
                                 //    link: $('a').attr('href'),
-                                //    img: $('img').attr('src'),
+                                opimg: $('img').attr('src'),
                                 //     replies: $('.meta > b:nth-child(1)').text(),
                                 //    imgReplies: $('.meta > b:nth-child(2)').text(),
                                 //     title: $('.teaser > b').first().text(),
-                                op: $('.teaser').text()
+                                op: $('.teaser').text(),
                                 // title: $('.postMessage').text()
+                                //date: $('span  > b:nth-child(8)').text(),
                             }
                         return data;
 
@@ -59,23 +62,52 @@ class chan {
 let chans = new chan()
 
 chans.crawlCatalogue().then(response => {
-    const flat= response.map(element => element.op);
-    cryptos = ['bitcoin','ethereum','tether','binance','cardano','dogecoin','xrp','polkadot','litecoin','vechain','monero','btc','eth','usdt','bnb','ada','doge','ripple','chainlink','link','vet','xmr','shib'];
-    let fin = [];
-    flat.forEach(element => {
-        element.replace(/\b((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))/g,'');
-        element.replace(/^(?:[a-z]*?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/gi,'');
-        cryptos.forEach(coin => {
-            if(element.toLowerCase().includes(coin))
+    let cryptos = ['bitcoin','ethereum','tether','binance','cardano','dogecoin','xrp','polkadot','litecoin','vechain','monero','btc','eth','usdt','bnb','ada','doge','ripple','chainlink','link','vet','xmr','shib'];
+    let fin = []
+    console.log(response)
+    for(let i=0;i<response.length;i++)
+    {
+
+
+            response[i].op.replace(/\b((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))/g,'');
+            response[i].op.replace(/^(?:[a-z]*?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/gi,'');
+
+
+        for(let x=0;x<cryptos.length;x++)
+        {
+
+            if(response[i].op.toLowerCase().includes(cryptos[x]))
             {
-                fin.push(element);
+                fin.push(response[i]);
             }
-        })
-    });
-    uniqueArray = fin.filter(function(item, pos) {
+        }
+    }
+    let uniqueArray = fin.filter(function(item, pos) {
         return fin.indexOf(item) == pos;
     })
     console.log(uniqueArray);
-    chans.firestore_db.save('4chan_data','biz','posts',empty);
-    chans.firestore_db.save('4chan_data','biz','posts',uniqueArray);
+
+    chans.firestore_db.save('4chan_info','biz','posts',empty);
+    chans.firestore_db.save('4chan_info','biz','posts',uniqueArray);
+
+
+    // const flat= response.map(element => element.op);
+    // cryptos = ['bitcoin','ethereum','tether','binance','cardano','dogecoin','xrp','polkadot','litecoin','vechain','monero','btc','eth','usdt','bnb','ada','doge','ripple','chainlink','link','vet','xmr','shib'];
+    // let fin = [];
+    // flat.forEach(element => {
+    //     element.replace(/\b((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))/g,'');
+    //     element.replace(/^(?:[a-z]*?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/gi,'');
+    //     cryptos.forEach(coin => {
+    //         if(element.toLowerCase().includes(coin))
+    //         {
+    //             fin.push(element);
+    //         }
+    //     })
+    // });
+    // uniqueArray = fin.filter(function(item, pos) {
+    //     return fin.indexOf(item) == pos;
+    // })
+    // console.log(uniqueArray);
+    // chans.firestore_db.save('4chan_data','biz','posts',empty);
+    // chans.firestore_db.save('4chan_data','biz','posts',uniqueArray);
 }).catch(e => console.log(e));

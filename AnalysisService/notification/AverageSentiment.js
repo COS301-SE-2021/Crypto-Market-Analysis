2efae398995399e4169e265d923f179b6ddf8366
Lerato <u18037951@tuks.co.za>
2021-07-29 19:03:48 +0200

    calculating change

--- AnalysisService/notification/AverageSentiment.js
+++ AnalysisService/notification/AverageSentiment.js
@@ -2,34 +2,30 @@ const Database = require('../database/Database');
 const firestore_db = new Database().getInstance();
 
 const Analyse_Average = async(SocialMedia,cryptocurrency)=>{
-   await firestore_db.getUsers(SocialMedia).onSnapshot((documents) => {
-       console.log(documents);
-        documents.forEach((doc) => {
-            if(typeof doc.id !== "undefined" && doc.id===cryptocurrency )
-            {
-                const difference = doc.data().Average- doc.data().Old_Average ;
-                console.log(doc.data().Old_Average)
-                console.log(difference)
-                if(difference < 0)
-                {
-                    return 'negative';
-                }
-               else if(difference > 0)
-                {
-                    return 'positive';
-                }
-               else
-                {
-                    return 'nothing';
+    const p =  new Promise(function (resolve, reject) {
+        firestore_db.getUsers(SocialMedia).onSnapshot(async (documents) => {
+            documents.forEach((doc) => {
+                if (typeof doc.id !== "undefined" && doc.id === cryptocurrency) {
+                    const difference = doc.data().Average - doc.data().Old_Average;
+                    console.log(doc.data().Average)
+                    console.log(doc.data().Old_Average);
+                    if (difference < 0) {
+                        return 'negative';
+                        resolve('negative');
+                    } else if (difference > 0) {
+                        resolve('positive');
+                    } else {
+                        resolve('nothing');
+                    }
+                    reject("error has occured")
                 }
 
-            }
-
+            })
         })
     })
+    return p;
 }
 Analyse_Average('Twitter','Bitcoin').then(data=>{
-    console.log('my data is here')
     console.log(data);
-})
+}).catch(err=>{console.log(err)})
 module.exports ={Analyse_Average };

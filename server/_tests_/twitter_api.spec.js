728e47b163d2914cdf15b81749e4f988f32696f9
Zee864 <82458469+Zee864@users.noreply.github.com>
2021-08-02 22:16:52 +0200

    Added integration tests for the route /twitter/getCryptoTweets

--- server/_tests_/twitter_api.spec.js
+++ server/_tests_/twitter_api.spec.js
@@ -2,6 +2,12 @@ const request = require('supertest');
 const app = require("../app");
 
 describe(`POST /twitter/getCryptoTweets`, () => {
+    test(`when parameters are correct`, async () => {
+        jest.setTimeout(10000);
+        const response = await request(app).post(`/twitter/getCryptoTweets`).send({email: `codexteam4@gmail.com`, crypto_name: `Bitcoin`});
+        expect(response.status).toBe(200);
+        expect(response.body.data).toBeDefined();
+    });
     test(`when email is not valid`, async () => {
         const response = await request(app).post(`/twitter/getCryptoTweets`).send({email: `fake@notvalid.com`, crypto_name: `Bitcoin`});
         expect(response.error.status).toBe(500);

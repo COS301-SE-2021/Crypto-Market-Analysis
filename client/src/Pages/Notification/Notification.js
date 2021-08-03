1af0586b33284082b5334641108ed139dfad7bd7
Lerato <u18037951@tuks.co.za>
2021-08-03 22:42:55 +0200

    mark messages

--- client/src/Pages/Notification/Notification.js
+++ client/src/Pages/Notification/Notification.js
@@ -8,12 +8,31 @@ import {
     Col,
 } from "react-bootstrap";
 import db from "../../firebase";
+import {Link} from "react-router-dom";
 
 function Notifications() {
     let[Notification_object,setNotification] =useState([]);
     let[elem,setElem] =useState([]);
     let[Objectdata,setObjData] =useState([]);
     const notificationAlertRef = React.useRef(null);
+    const handleView= async (e)=> {
+         // e.preventDefault();
+          console.log('view has been clicked')
+        await db.firestore().collection('Users').doc(localStorage.getItem("emailSession")).get().then((notify)=> {
+            console.log(e)
+            let viewObject = notify.data().notification;
+            console.log(viewObject);
+            if(viewObject[e]!=='undefined'){
+                viewObject[e].Read =true;
+            }
+
+            const notification_view_object ={
+                notification: viewObject
+            }
+            db.firestore().collection('Users').doc(localStorage.getItem("emailSession")).set(notification_view_object,
+                {merge: true});
+        })
+    }
    const handleDelete= async (e)=> {
        console.log('delete pressed');
         e.preventDefault();
@@ -44,17 +63,28 @@ function Notifications() {
             let i = 0;
             for (const [key, value] of Object.entries(notify.data().notification)) {
                 i=i+1;
+                console.log(key)
                 //console.log(`${key}: ${value.Email}`);
                 notification_Array.push( <Alert variant="info">
+                    <Link  onClick={()=>handleView(key)} value={key}
+                          className={
+                            "text-xs uppercase py-3 font-bold block " +
+                            (window.location.href.indexOf("/Profile") !== -1
+                                ? "text-lightBlue-500 hover:text-lightBlue-600"
+                                : "text-blueGray-700 hover:text-blueGray-500")
+                        }
+                        to="/Profile"
+                    >
                     <i className="far fa-bell"></i>
                     <span>
                                         <p className="text-success"><h3>Cryptocurrency Notification {Notification_object.Email}</h3></p><p>{key}</p>
 
                                           {value.Email}
                         <br></br>
-                                                 <button  onClick={handleDelete} value={key} type="button" className="btn btn-outline-warning"><i className="fas fa-trash-alt"></i>Delete</button>
-                                    </span>
 
+                                                 <button  onClick={handleDelete} value={key} type="button" className="btn btn-outline-warning"><i className="fas fa-trash-alt"></i>Delete</button>
+                    </span>
+                    </Link>
                 </Alert>)
                 if(i === Object.entries(notify.data().notification).length){
                     setElem(notification_Array);

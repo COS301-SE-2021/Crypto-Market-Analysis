8a1d99bcac0403b9e3b565946883876c5b7abe71
Bheki Ndhlovu <bhekindhlovu7@gmail.com>
2021-08-04 15:28:54 +0200

    reddit

--- client/src/Pages/DetailedInfo/DetailedInfo.js
+++ client/src/Pages/DetailedInfo/DetailedInfo.js
@@ -24,15 +24,15 @@ export default function DetailedInfo(props) {
                         <Tab eventKey="Tweets" title="Tweets">
                             <Tweets coin_name={coin_name} />
                         </Tab>
-                        <Tab eventKey="Reddit" title="Reddit">
-                            <Reddits />
-                        </Tab>
+                        {/*<Tab eventKey="Reddit" title="Reddit">*/}
+                        {/*    <Reddits />*/}
+                        {/*</Tab>*/}
                         <Tab eventKey="4chan" title="4chan">
                             <FourChan />
                         </Tab>
-                        {/*<Tab eventKey="Reddit" title="Reddit">*/}
-                        {/*    <Reddit coin_name={coin_name} />*/}
-                        {/*</Tab>*/}
+                        <Tab eventKey="Reddit" title="Reddit">
+                            <Reddit coin_name={coin_name} />
+                        </Tab>
                     </Tabs>
                 </div>
             </div>

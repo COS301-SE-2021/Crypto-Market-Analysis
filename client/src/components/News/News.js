7e9adbea652720a6bc67c44e29131ec3876887a8
Zee864 <82458469+Zee864@users.noreply.github.com>
2021-08-05 21:34:07 +0200

    Added news component as well as fixed several bugs and errors on the home page as well as on the register and profile pages

---
+++ client/src/components/News/News.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+export default function News() {
+    useEffect(() => {
+        /*axios.get('https://cryptopanic.com/api/v1/posts/?auth_token=585adc3fa49b85db0639c797c97deec94c9901da&public=true&currencies=conv&kind=news')
+            .then(response => console.log(response));*/
+    }, []);
+    return (<></>)
+}

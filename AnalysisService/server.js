8f24cc348b97602f35de1442173a65fb4288d19b
Lerato <u18037951@tuks.co.za>
2021-08-09 18:37:06 +0200

    updated

--- AnalysisService/server.js
+++ AnalysisService/server.js
@@ -1,19 +1,20 @@
 const express = require('express')
 const https = require('https')
 const path = require('path')
+const bodyParser = require("body-parser");
 const fs = require('fs')
 const dotenv = require('dotenv');
+
 const cron = require('node-cron');
 const analysis = require('./analysisFunction');
 const notification = require('./notification/notification');
 const notificationType =require('./notification/notificationType')
-const Push_notification=require('./notification/push_notification')
+
 const average = require('./notification/AverageSentiment')
 const app = express()
 
 app.use('/', (req, res, next) => {
     res.send('SSL server started')
-
 })
 
 const sslServer = https.createServer(
@@ -25,21 +26,22 @@ const sslServer = https.createServer(
 )
 
 sslServer.listen(3443, () => console.log('Secure server running on port 3443'))
-let push = new Push_notification();
 
-cron.schedule('*/1 * * * *', async () => {
-   console.log('Analysing every minute');
-    await analysis.sentimentAnalysis('Bitcoin','Twitter').then(data=>{
-        console.log('outputting data');
-        console.log(data);
-        average.Analyse_Average('Twitter','Bitcoin').then(dt=>{
-            console.log('showing data results');
-            console.log(dt);
-            let msgType = new notificationType(dt,'Bitcoin');
-            let results = msgType.Results();
-            notification.followers('Bitcoin',results);
-        });
+//let push = new Push_notification();
 
-    }).catch(err=>{   console.log('Error in Sentiment Analysis');})
-    console.log('Analysing done!');
-});
+// cron.schedule('*/1 * * * *', async () => {
+//   console.log('Analysing every minute');
+//     await analysis.sentimentAnalysis('Bitcoin','Twitter').then(data=>{
+//         console.log('outputting data');
+//         console.log(data);
+//         average.Analyse_Average('Twitter','Bitcoin').then(dt=>{
+//             console.log('showing data results');
+//             console.log(dt);
+//             let msgType = new notificationType(dt,'Bitcoin');
+//             let results = msgType.Results();
+//             notification.followers('Bitcoin',results);
+//         });
+//
+//     }).catch(err=>{   console.log('Error in Sentiment Analysis');})
+//     console.log('Analysing done!');
+// });

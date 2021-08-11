345c0f90ab1d7f27587afc179201bd6571bbaef9
Zee864 <82458469+Zee864@users.noreply.github.com>
2021-08-11 20:37:04 +0200

    Modified getAverages method to include values from the database

--- server/functions/sentiment_functions.js
+++ server/functions/sentiment_functions.js
@@ -2,29 +2,32 @@ const Database = require(`../database/Database`);
 const firestore_database = new Database().getInstance();
 
 const getAverages = async (email, crypto_name) => {
-    //let averages = {};
+    let averages = [];
 
     try{
         const social_media_sites = await firestore_database.fetch(`Users`, email, `social_media_sites`);
         if(social_media_sites){
-            /*for(const platform of social_media_sites){
+            for(const platform of social_media_sites){
                 try{
-                    averages[`${platform}_Averages`] = await firestore_database.fetch(platform, crypto_name, `Average`);
+                    let average = await firestore_database.fetch(platform, crypto_name, `Average_Change`);
+                    if(average) {
+                        if(averages.length > 0){
+                            for(const [index,element] of average.entries()) {
+                                if(averages[index])
+                                    averages[index] = Math.round(((element + averages[index]) / 2) * 100) / 100;
+                            }
+                        }
+                        else
+                            averages = averages.concat(average);
+                    }
                 }
                 catch (error) {
                     return Promise.reject(error);
                 }
             }
 
-            return averages;*/
-            let iterations = Math.floor(Math.random() * (24 - 1) + 1);
-            let averages = [];
-            while(iterations > 0) {
-                let random_number = Math.random() * (5 - (-5)) + (-5);
-                let average = Math.round((random_number+ Number.EPSILON) * 100) / 100;
-                averages.push(average);
-                iterations--;
-            }
+            if(averages.length < 0)
+                return Promise.reject(`Not enough data at the moment to plot the graph. Check again later.`);
 
             return averages;
         }
@@ -35,5 +38,7 @@ const getAverages = async (email, crypto_name) => {
         return Promise.reject(error);
     }
 };
+
+getAverages(`alekarzeeshan92@gmail.com`, `Bitcoin`).then(res => console.log(res));
 
 module.exports = {getAverages};

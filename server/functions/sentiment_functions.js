8442ca59389076c7e1da4a5ac83b1f6c9b6a08b4
Zee864 <82458469+Zee864@users.noreply.github.com>
2021-08-11 18:09:27 +0200

    Partially implement getAverages function

---
+++ server/functions/sentiment_functions.js
@@ -0,0 +1,8 @@
+const Database = require(`../database/Database`);
+const firestore_database = new Database().getInstance();
+
+const getAverages = () => {
+    
+};
+
+module.exports = {getAverages};
--- server/routes/sentiment.js
+++ server/routes/sentiment.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.post('/getAverages', async (request, response, next) => {
-    
+
 });
 
 module.exports = router

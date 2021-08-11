2d948dfd0d55b7fffc6ed6b90f33190f9c1a8fb1
Lerato <u18037951@tuks.co.za>
2021-08-11 12:36:55 +0200

    removed db

--- client/src/Pages/Push/Push.js
+++ client/src/Pages/Push/Push.js
@@ -23,6 +23,32 @@ class Push extends React.Component {
         this.handleSubscribe = this.handleSubscribe.bind(this);
     }
     handleSubscribe= async (e)=> {
+        let  PushReq = {
+            email: localStorage.getItem("emailSession")
+        }
+        axios.post('http://localhost:8080/user/GETPush/',PushReq)
+            .then(response => {
+                console.log(response.data)
+                if(Object.keys(response.data).length !== 0)
+                {
+                    const notification_Arrays = [];
+                    notification_Arrays.push(<Col class="col-md-6 offset-md-4">
+                        <button  onClick={this.handleSubscribe}  type="button" className="btn btn-outline-warning">
+                            Subscribed</button>
+                    </Col>);
+                    this.setState({elements: notification_Arrays});
+                }
+                else{
+                    const notificationpush= [];
+                    notificationpush.push(<Col class="col-md-6 offset-md-4">
+                        <button  onClick={this.handleSubscribe}  type="button" className="btn btn-outline-warning">
+                            Subscribe</button>
+                    </Col>);
+                    this.setState({elements: notificationpush});
+                }
+
+            })
+            .catch(err => {console.error(err);})
         const publicVapidKey =
             "BC0gVsYz3ljCKUO5KCfEcpaTFj3Ye_Q_q8vAWBTKDd_53jXsvtRMOYqTCUggv62UE3As_8psAJwXwTKTgXYUKNs";
         if ("serviceWorker" in navigator) {
@@ -83,60 +109,13 @@ class Push extends React.Component {
 
     }
     componentDidMount(){
-        let  PushReq = {
-            email: localStorage.getItem("emailSession")
-        }
-        axios.post('http://localhost:8080/user/GETPush/',PushReq)
-            .then(response => {
-                console.log(response.data)
-                if(Object.keys(response.data).length !== 0)
-                {
-                    const notification_Arrays = [];
-                    notification_Arrays.push(<Col class="col-md-6 offset-md-4">
-                        <button  onClick={this.handleSubscribe}  type="button" className="btn btn-outline-warning">
-                            Subscribed</button>
-                    </Col>);
-                    this.setState({elements: notification_Arrays});
-                }
-                else{
-                    const notificationpush= [];
-                    notificationpush.push(<Col class="col-md-6 offset-md-4">
-                        <button  onClick={this.handleSubscribe}  type="button" className="btn btn-outline-warning">
-                            Subscribe</button>
-                    </Col>);
-                    this.setState({elements: notificationpush});
-                }
-
-            })
-            .catch(err => {console.error(err);})
-
-
-
-
 
     }
-
     render() {
         return (
-            <><Sidebar />
+            <>
                 <div className="md:ml-64">
-
-                    <Container fluid>
-
-                        <Card>
-                            <Card.Header>
-                                <Card.Title as="h4">Notifications</Card.Title>
-                            </Card.Header>
-                            <Card.Body>
-                                <Row>
-                                    {this.state.elements}
-                                </Row>
-
-                            </Card.Body>
-                        </Card>
-
-
-                    </Container>
+                         {this.state.elements}
                 </div>
             </>
         );

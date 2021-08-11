76ae2bd130630fefe04c9300f821bcc588c7ee19
Zee864 <82458469+Zee864@users.noreply.github.com>
2021-08-11 18:30:03 +0200

    Implemented getAverages function

--- server/functions/sentiment_functions.js
+++ server/functions/sentiment_functions.js
@@ -2,22 +2,20 @@ const Database = require(`../database/Database`);
 const firestore_database = new Database().getInstance();
 
 const getAverages = async (email, crypto_name) => {
+    let averages = {};
+
     try{
         const social_media_sites = await firestore_database.fetch(`Users`, email, `social_media_sites`);
         if(social_media_sites){
             for(const platform of social_media_sites){
                 try{
-                    const averages = await firestore_database.fetch(platform, crypto_name, `Average_Change`);
-                    if(averages){
-
-                    }
-                    else
-                        return Promise.reject(`Data is insufficient to plot the graph. Please check again later.`);
+                    averages[`${platform}_Averages`] = await firestore_database.fetch(platform, crypto_name, `Average_Change`);
                 }
                 catch (error) {
                     return Promise.reject(error);
                 }
             }
+            return averages;
         }
         else
             return Promise.reject(`You are not following any platforms. Please follow at least one platform to view the graph.`)

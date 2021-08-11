35e6d09d2b8e3c684dc44a8abc6a039e06a06a1d
Zee864 <82458469+Zee864@users.noreply.github.com>
2021-08-11 18:18:27 +0200

    Added data retrieval for sentiment_functions.js

--- server/functions/sentiment_functions.js
+++ server/functions/sentiment_functions.js
@@ -1,7 +1,7 @@
 const Database = require(`../database/Database`);
 const firestore_database = new Database().getInstance();
 
-const getAverages = async (crypto_name) => {
+const getAverages = async (crypto_name, social_media_site) => {
     const averages = await firestore_database.fetch();
 };
 
--- server/routes/sentiment.js
+++ server/routes/sentiment.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const sentiment_functions = require(`../functions/sentiment_functions`);
 
 router.post('/getAverages', async (request, response, next) => {
     const {crypto_name, social_media_site} = request.body;
@@ -9,6 +10,17 @@ router.post('/getAverages', async (request, response, next) => {
         let error = new Error(`Malformed request. Please check your parameters`);
         error.status = 400;
         return next(error);
+    }
+    else{
+        try{
+            const data = await sentiment_functions.getAverages(crypto_name, social_media_site);
+            return response.status(200).json({data: data});
+        }
+        catch (error){
+            let error = new Error(err);
+            error.status = 500;
+            return next(error);
+        }
     }
 
 });

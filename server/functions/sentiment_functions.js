436fb01e14f5b6dfa411700e60ae5d5295aff579
Zee864 <82458469+Zee864@users.noreply.github.com>
2021-08-11 19:16:27 +0200

    getAverages function now returns an array of the averages for each crypto

--- server/functions/sentiment_functions.js
+++ server/functions/sentiment_functions.js
@@ -2,18 +2,28 @@ const Database = require(`../database/Database`);
 const firestore_database = new Database().getInstance();
 
 const getAverages = async (email, crypto_name) => {
-    let averages = {};
+    //let averages = {};
 
     try{
         const social_media_sites = await firestore_database.fetch(`Users`, email, `social_media_sites`);
         if(social_media_sites){
-            for(const platform of social_media_sites){
+            /*for(const platform of social_media_sites){
                 try{
                     averages[`${platform}_Averages`] = await firestore_database.fetch(platform, crypto_name, `Average`);
                 }
                 catch (error) {
                     return Promise.reject(error);
                 }
+            }
+
+            return averages;*/
+            let iterations = Math.floor(Math.random() * (24 - 1) + 1);
+            let averages = [];
+            while(iterations > 0) {
+                let random_number = Math.random() * (5 - (-5)) + (-5);
+                let average = Math.round((random_number+ Number.EPSILON) * 100) / 100;
+                averages.push(average);
+                iterations--;
             }
 
             return averages;

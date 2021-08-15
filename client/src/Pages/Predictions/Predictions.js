b5433b17810875cf739d88f6a7bdea1ae7badbad
Bheki Ndhlovu <bhekindhlovu7@gmail.com>
2021-08-15 17:16:28 +0200

    loading screen

--- client/src/Pages/Predictions/Predictions.js
+++ client/src/Pages/Predictions/Predictions.js
@@ -1,20 +1,24 @@
 import './Predictions.scss';
 import "bootstrap/dist/css/bootstrap.css";
+import Sidebar from "../../components/Sidebar/Sidebar";
 import { Star, } from "@material-ui/icons";
 import React, { useState, useEffect } from 'react';
 import axios from "axios";
+import ClipLoader from "react-spinners/ClipLoader"
 const CoinGecko = require('coingecko-api');
 const CoinGeckoClient = new CoinGecko();
 
 function Predictions() {
     let [predictions,setPredictions] = useState([]);
     let PredictionsCoins = [];
-
+    let [loading, setLoading] = useState(true);
     useEffect(async () => {
+        console.log("use effect")
         let userReq = { email: localStorage.getItem("emailSession") }
         let allcoins = await CoinGeckoClient.coins.all();
         axios.post('http://localhost:8080/user/getCoinPredictions/',userReq)
             .then(async(response) =>{
+                console.log("getCoinPredictions")
                 for(let y=0;y<allcoins.data.length;y++)
                 {
                     for(let x=0;x<response.data.posts_array.length;x++)
@@ -27,6 +31,7 @@ function Predictions() {
                             let name = allcoins.data[y].name;
                             await axios.get('https://min-api.cryptocompare.com/data/price?fsym=' + val + '&tsyms=USD&api_key=7d4a73a2b7a6fd2e5d57acd8c019cb82178961644e25b7caad3239d04e79da4b')
                                 .then(async(price) =>{
+                                    console.log("price")
                                     let obj = {
                                         picture: picture,
                                         name: name,
@@ -38,13 +43,13 @@ function Predictions() {
                                         high: response.data.posts_array[x].high,
                                     }
                                     PredictionsCoins.push(obj);
-                                })
+                                },reject=>{console.log(reject)})
                                 .catch(err => {console.error(err)})
 
                         }
                     }
                 }
-                console.log("test");
+
                 var resArr = [];
                 PredictionsCoins.forEach(function(item){
                     var i = resArr.findIndex(x => x.name == item.name);
@@ -52,50 +57,56 @@ function Predictions() {
                         resArr.push({name: item.name, symbol: item.symbol, price: item.price, open: item.open, close: item.close, low: item.low});
                     }
                 });
-                //console.log(resArr);
+                console.log("test");
+                console.log(resArr);
                 console.log("test");
                 setPredictions(resArr);
+                setLoading(false)
             })
             .catch(err => {console.error(err)})
     },[]);
 
     return(
         <>
+            <Sidebar></Sidebar>
             <div className="container-fluid" >
                 <div className="row">
                     <div className="col-md-12">
-                        <table className="table crypto-table">
-                            <thead>
-                            <tr>
-                                <th scope="col">#</th>
-                                <th scope="col">Name</th>
-                                <th scope="col">Symbol</th>
-                                <th scope="col">Price</th>
-                                <th scope="col">Prediction</th>
-                            </tr>
-                            </thead>
-                            <tbody>
 
-                            {
-                                predictions.map((i) =>{
-                                    // console.dir(i);
-                                    return(
-                                        // <p>{i.name}
-                                        //     <p>{i.price}</p>
-                                        // </p>
-                                        <tr>
-                                            <td>#</td>
-                                            <td><span class="text-warning">{i.name}</span></td>
-                                            <td>{i.symbol.toUpperCase()}</td>
-                                            <td class="text-warning">{i.price}</td>
-                                            <td class="text-warning">{i.open}</td>
-                                        </tr>
-                                    )
-                                })
-                            }
+                        {loading ? <div className="mx-auto mt-8 text-center"><ClipLoader loading={loading} size={150} /></div>:
 
-                            </tbody>
-                        </table>
+                            <table className="table crypto-table">
+                                <thead>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Name</th>
+                                    <th scope="col">Symbol</th>
+                                    <th scope="col">Price</th>
+                                    <th scope="col">Prediction</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                {
+                                    predictions.map((i) =>{
+                                        // console.dir(i);
+                                        return(
+                                            // <p>{i.name}
+                                            //     <p>{i.price}</p>
+                                            // </p>
+                                            <tr>
+                                                <td>#</td>
+                                                <td><span class="text-warning">{i.name}</span></td>
+                                                <td>{i.symbol.toUpperCase()}</td>
+                                                <td class="text-warning">{i.price}</td>
+                                                <td class="text-warning">{i.open}</td>
+                                            </tr>
+                                        )
+                                    })
+                                }
+                                </tbody>
+                            </table>
+                        }
+
                     </div>
                 </div>
             </div>
@@ -106,4 +117,5 @@ function Predictions() {
 }
 
 export default Predictions;
+
 

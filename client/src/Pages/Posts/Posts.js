6ac51fb0c7587337b7c43c424d55ac12b3a342eb
Bheki Ndhlovu <bhekindhlovu7@gmail.com>
2021-08-31 15:43:08 +0200

    Posts

--- client/src/Pages/Posts/Posts.js
+++ client/src/Pages/Posts/Posts.js
@@ -17,7 +17,19 @@ function Posts() {
 
         axios.post('http://localhost:8080/chat/getAllChats/',Req)
             .then(response => {
-               console.log(response);
+
+
+                let posts_ = [];
+                for(let j = 0; j<response.data.posts_array.length; j++)
+                {
+
+                        posts_.push(response.data.posts_array[j])
+
+
+                }
+                console.log(posts_)
+
+                setposts(posts_);
             })
             .catch(err => {console.error(err);})
         setTimeout(()=>{
@@ -28,51 +40,66 @@ function Posts() {
     return(
 
         <>
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-8">
-                            <div className="media g-mb-30 media-comment">
-                                    <div className="media-body u-shadow-v18 g-bg-secondary g-pa-30">
-                                        <div className="g-mb-15">
-                                            <h5 className="h5 g-color-gray-dark-v1 mb-0">John Doe</h5>
-                                            <span className="g-color-gray-dark-v4 g-font-size-12">5 days ago</span>
-                                        </div>
+            {
+                posts.map((post) =>{
 
-                                        <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante
-                                            sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra
-                                            turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia
-                                            congue
-                                            felis in faucibus ras purus odio, vestibulum in vulputate at, tempus viverra
-                                            turpis.</p>
-
-                                        <ul className="list-inline d-sm-flex my-0">
-                                            <li className="list-inline-item g-mr-20">
-                                                <a className="u-link-v5 g-color-gray-dark-v4 g-color-primary--hover"
-                                                   href="#!">
-                                                    <i className="fa fa-thumbs-up g-pos-rel g-top-1 g-mr-3"></i>
-                                                    178
-                                                </a>
-                                            </li>
-                                            <li className="list-inline-item g-mr-20">
-                                                <a className="u-link-v5 g-color-gray-dark-v4 g-color-primary--hover"
-                                                   href="#!">
-                                                    <i className="fa fa-thumbs-down g-pos-rel g-top-1 g-mr-3"></i>
-                                                    34
-                                                </a>
-                                            </li>
-                                            <li className="list-inline-item ml-auto">
-                                                <a className="u-link-v5 g-color-gray-dark-v4 g-color-primary--hover"
-                                                   href="#!">
-                                                    <i className="fa fa-reply g-pos-rel g-top-1 g-mr-3"></i>
-                                                    Reply
-                                                </a>
-                                            </li>
-                                        </ul>
-                                    </div>
+                    return(
+                        <div className="card mb-3">
+                            <div className="card-body">
+                                <p className="card-text">{post.title}</p>
+                                <p className="card-text">
+                                    <small className="text-muted">posted on the /biz board</small>
+                                </p>
                             </div>
                         </div>
-                    </div>
-                </div>
+                    )
+                })
+            }
+                {/*<div className="container">*/}
+                {/*    <div className="row">*/}
+                {/*        <div className="col-md-8">*/}
+                {/*            <div className="media g-mb-30 media-comment">*/}
+                {/*                    <div className="media-body u-shadow-v18 g-bg-secondary g-pa-30">*/}
+                {/*                        <div className="g-mb-15">*/}
+                {/*                            <h5 className="h5 g-color-gray-dark-v1 mb-0">John Doe</h5>*/}
+                {/*                            <span className="g-color-gray-dark-v4 g-font-size-12">5 days ago</span>*/}
+                {/*                        </div>*/}
+
+                {/*                        <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante*/}
+                {/*                            sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra*/}
+                {/*                            turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia*/}
+                {/*                            congue*/}
+                {/*                            felis in faucibus ras purus odio, vestibulum in vulputate at, tempus viverra*/}
+                {/*                            turpis.</p>*/}
+
+                {/*                        <ul className="list-inline d-sm-flex my-0">*/}
+                {/*                            <li className="list-inline-item g-mr-20">*/}
+                {/*                                <a className="u-link-v5 g-color-gray-dark-v4 g-color-primary--hover"*/}
+                {/*                                   href="#!">*/}
+                {/*                                    <i className="fa fa-thumbs-up g-pos-rel g-top-1 g-mr-3"></i>*/}
+                {/*                                    178*/}
+                {/*                                </a>*/}
+                {/*                            </li>*/}
+                {/*                            <li className="list-inline-item g-mr-20">*/}
+                {/*                                <a className="u-link-v5 g-color-gray-dark-v4 g-color-primary--hover"*/}
+                {/*                                   href="#!">*/}
+                {/*                                    <i className="fa fa-thumbs-down g-pos-rel g-top-1 g-mr-3"></i>*/}
+                {/*                                    34*/}
+                {/*                                </a>*/}
+                {/*                            </li>*/}
+                {/*                            <li className="list-inline-item ml-auto">*/}
+                {/*                                <a className="u-link-v5 g-color-gray-dark-v4 g-color-primary--hover"*/}
+                {/*                                   href="#!">*/}
+                {/*                                    <i className="fa fa-reply g-pos-rel g-top-1 g-mr-3"></i>*/}
+                {/*                                    Reply*/}
+                {/*                                </a>*/}
+                {/*                            </li>*/}
+                {/*                        </ul>*/}
+                {/*                    </div>*/}
+                {/*            </div>*/}
+                {/*        </div>*/}
+                {/*    </div>*/}
+                {/*</div>*/}
 
         </>
     )

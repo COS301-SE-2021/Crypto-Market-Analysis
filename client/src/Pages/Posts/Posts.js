cce4b0c1240553a272c43b28edd1c818ec76981c
Bheki Ndhlovu <bhekindhlovu7@gmail.com>
2021-08-31 16:48:08 +0200

    create post display lists of posts

--- client/src/Pages/Posts/Posts.js
+++ client/src/Pages/Posts/Posts.js
@@ -1,14 +1,46 @@
 import "bootstrap/dist/css/bootstrap.css";
 import './Posts.css';
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import axios from "axios";
 import Sidebar from "../../components/Sidebar/Sidebar";
+import { Form, Button, Card, Alert } from "react-bootstrap"
+import { Link, useHistory } from "react-router-dom"
 
 
 function Posts() {
 
     let [posts,setposts] = useState([]);
+
+    const history = useHistory()
+    const title = useRef();
+    const body = useRef();
+    const time = new Date().toLocaleString();
+
+    function handleSubmit(e) {
+        e.preventDefault()
+
+        let request = {
+            owner: "bhekindhlovu7@gmail.com",
+            room: "Altcoins",
+            title: title.current.value,
+            body: body.current.value,
+            time: time
+        };
+
+        axios.post('http://localhost:8080/chat/postMessage/',request)
+            .then(response => {
+                console.log(response);
+                // history.push("/");
+                window.location.reload();
+            })
+            .catch(err => {console.error(err);})
+        setTimeout(()=>{
+        },10000)
+
+    }
+
     //must get room from prop but its default for now
+    //must get email from session
     useEffect( () => {
         let  Req = {
             owner: "bhekindhlovu7@gmail.com",
@@ -40,66 +72,84 @@ function Posts() {
     return(
 
         <>
+            <Card>
+                <Card.Body>
+                    <h2 className="text-center mb-4">Create Form</h2>
+                    <Form onSubmit={handleSubmit}>
+
+                        <Form.Group id="title">
+                            <Form.Label>title</Form.Label>
+                            <Form.Control
+                                ref={title}
+                                required
+                            />
+                        </Form.Group>
+
+                        <Form.Group id="body">
+                            <Form.Label>body</Form.Label>
+                            <Form.Control
+                                ref={body}
+                                required
+                            />
+                        </Form.Group>
+
+
+                        <Button className="w-100" type="submit">
+                            <Link to="/Posts">Post</Link>
+                        </Button>
+                    </Form>
+                </Card.Body>
+            </Card>
+
             {
                 posts.map((post) =>{
 
                     return(
-                        <div className="card mb-3">
-                            <div className="card-body">
-                                <p className="card-text">{post.title}</p>
-                                <p className="card-text">
-                                    <small className="text-muted">posted on the /biz board</small>
-                                </p>
+
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-8">
+                            <div className="media g-mb-30 media-comment">
+                                    <div className="media-body u-shadow-v18 g-bg-secondary g-pa-30">
+                                        <div className="g-mb-15">
+                                            <h5 className="h5 g-color-gray-dark-v1 mb-0">{post.title}</h5>
+                                            <span className="g-color-gray-dark-v4 g-font-size-12">{post.time}</span>
+                                        </div>
+
+                                        <p>{post.body}</p>
+
+                                        <ul className="list-inline d-sm-flex my-0">
+                                            <li className="list-inline-item g-mr-20">
+                                                <a className="u-link-v5 g-color-gray-dark-v4 g-color-primary--hover"
+                                                   href="#!">
+                                                    <i className="fa fa-thumbs-up g-pos-rel g-top-1 g-mr-3"></i>
+                                                    {post.like}
+                                                </a>
+                                            </li>
+                                            <li className="list-inline-item g-mr-20">
+                                                <a className="u-link-v5 g-color-gray-dark-v4 g-color-primary--hover"
+                                                   href="#!" >
+                                                    <i className="fa fa-thumbs-down g-pos-rel g-top-1 g-mr-3"></i>
+                                                    {post.dislike}
+                                                </a>
+                                            </li>
+                                            <li className="list-inline-item ml-auto">
+                                                <a className="u-link-v5 g-color-gray-dark-v4 g-color-primary--hover"
+                                                   href="#!">
+                                                    <i className="fa fa-reply g-pos-rel g-top-1 g-mr-3"></i>
+                                                    Reply
+                                                </a>
+                                            </li>
+                                        </ul>
+                                    </div>
                             </div>
                         </div>
-                    )
+                    </div>
+                </div>
+
+                )
                 })
             }
-                {/*<div className="container">*/}
-                {/*    <div className="row">*/}
-                {/*        <div className="col-md-8">*/}
-                {/*            <div className="media g-mb-30 media-comment">*/}
-                {/*                    <div className="media-body u-shadow-v18 g-bg-secondary g-pa-30">*/}
-                {/*                        <div className="g-mb-15">*/}
-                {/*                            <h5 className="h5 g-color-gray-dark-v1 mb-0">John Doe</h5>*/}
-                {/*                            <span className="g-color-gray-dark-v4 g-font-size-12">5 days ago</span>*/}
-                {/*                        </div>*/}
-
-                {/*                        <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante*/}
-                {/*                            sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra*/}
-                {/*                            turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia*/}
-                {/*                            congue*/}
-                {/*                            felis in faucibus ras purus odio, vestibulum in vulputate at, tempus viverra*/}
-                {/*                            turpis.</p>*/}
-
-                {/*                        <ul className="list-inline d-sm-flex my-0">*/}
-                {/*                            <li className="list-inline-item g-mr-20">*/}
-                {/*                                <a className="u-link-v5 g-color-gray-dark-v4 g-color-primary--hover"*/}
-                {/*                                   href="#!">*/}
-                {/*                                    <i className="fa fa-thumbs-up g-pos-rel g-top-1 g-mr-3"></i>*/}
-                {/*                                    178*/}
-                {/*                                </a>*/}
-                {/*                            </li>*/}
-                {/*                            <li className="list-inline-item g-mr-20">*/}
-                {/*                                <a className="u-link-v5 g-color-gray-dark-v4 g-color-primary--hover"*/}
-                {/*                                   href="#!">*/}
-                {/*                                    <i className="fa fa-thumbs-down g-pos-rel g-top-1 g-mr-3"></i>*/}
-                {/*                                    34*/}
-                {/*                                </a>*/}
-                {/*                            </li>*/}
-                {/*                            <li className="list-inline-item ml-auto">*/}
-                {/*                                <a className="u-link-v5 g-color-gray-dark-v4 g-color-primary--hover"*/}
-                {/*                                   href="#!">*/}
-                {/*                                    <i className="fa fa-reply g-pos-rel g-top-1 g-mr-3"></i>*/}
-                {/*                                    Reply*/}
-                {/*                                </a>*/}
-                {/*                            </li>*/}
-                {/*                        </ul>*/}
-                {/*                    </div>*/}
-                {/*            </div>*/}
-                {/*        </div>*/}
-                {/*    </div>*/}
-                {/*</div>*/}
 
         </>
     )

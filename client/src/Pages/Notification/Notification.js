d9c421adba62da1f20184bf9fd34a1213b3bea38
kg <kgothalang451@gmail.com>
2021-09-16 12:57:35 +0200

    Update Notification.js

--- client/src/Pages/Notification/Notification.js
+++ client/src/Pages/Notification/Notification.js
@@ -13,6 +13,7 @@ import Sidebar from "../../components/Sidebar/Sidebar";
 import Push from "../Push/Push";
 import Checkbox from '@material-ui/core/Checkbox';
 import SweetAlert from 'sweetalert-react';
+import swal from 'sweetalert';
 import DayPickerInput from 'react-day-picker/DayPickerInput';
 import DayPicker from 'react-day-picker';
 import 'react-day-picker/lib/style.css';

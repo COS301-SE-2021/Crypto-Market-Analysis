ada182c6f92f58ff86045f7caf85157efd1be39a
Johannes <u18037951@tuks.co.za>
2021-09-18 19:43:57 +0200

    added scrape schedule

--- server/Schedules/Scrapper.js
+++ server/Schedules/Scrapper.js
@@ -1,5 +1,6 @@
 const Reddit = require('../functions/Reddit');
 const Twitter = require(`../functions/Twitter`);
+const chan = require("../functions/chan");
 const twitter = new Twitter().getInstance();
 let reddits = new Reddit();
 const scrape=async ()=>{
@@ -17,6 +18,32 @@ const scrape=async ()=>{
     reddits.completeScrape("Crypto_Currency_News").then();
     reddits.completeScrape("CryptoCurrencyTrading").then();
     reddits.completeScrape("Cryptomarkets").then();
+    let chans = new chan()
+    chans.crawlCatalogue().then(response => {
+        let cryptos = ['bitcoin','ethereum','tether','binance','cardano','dogecoin','xrp','polkadot','litecoin','vechain','monero','btc','eth','usdt','bnb','ada','doge','ripple','chainlink','link','vet','xmr','shib'];
+        let fin = []
+        for(let i=0;i<response.length;i++)
+        {
+
+            response[i].opimg= "https:"+response[i].opimg;
+            response[i].op.replace(/\b((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))/g,'');
+            response[i].op.replace(/^(?:[a-z]*?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/gi,'');
+            for(let x=0;x<cryptos.length;x++)
+            {
+
+                if(response[i].op.toLowerCase().includes(cryptos[x]))
+                {
+                    fin.push(response[i]);
+                }
+            }
+        }
+        let uniqueArray = fin.filter(function(item, pos) {
+            return fin.indexOf(item) == pos;
+        })
+        let empty = []
+        chans.firestore_db.save('4chan_info','biz','posts',empty);
+        chans.firestore_db.save('4chan_info','biz','posts',uniqueArray);
+    }).catch(e => console.log(e));
     process.exit(0);
 }
 

6e5eed5c91c1f741da80b139c5e80276ebea447e
MelussiMkhonta <u14079412@tuks.co.za>
2021-09-25 22:57:01 +0200

    Added Portfolio

--- client/src/components/Sidebar/Sidebar.js
+++ client/src/components/Sidebar/Sidebar.js
@@ -205,6 +205,29 @@ export default function Sidebar(props) {
                   </Link>
                 </li>
 
+                  <li className="items-center">
+                      <Link
+                          className={
+                              "text-xs uppercase py-3 font-bold block " +
+                              (window.location.href.indexOf("/home") !== -1
+                                  ? "text-lightBlue-500 hover:text-lightBlue-600"
+                                  : "text-blueGray-700 hover:text-blueGray-500")
+                          }
+                          to="/Portfolio"
+                          onClick={changeLocation}
+                      >
+                          <i
+                              className={
+                                  "fas fa-tv mr-2 text-sm " +
+                                  (window.location.href.indexOf("/Portfolio") !== -1
+                                      ? "opacity-75"
+                                      : "text-blueGray-300")
+                              }
+                          />{" "}
+                          Portfolio
+                      </Link>
+                  </li>
+
                 <li className="items-center">
                   <Link
                       className={

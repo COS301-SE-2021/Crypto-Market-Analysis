b1d43fb182cdac4216d3fef2f818ff08ada29c8c
Johannes <u18037951@tuks.co.za>
2021-09-28 16:45:20 +0200

    portfolio post

--- server/routes/user.js
+++ server/routes/user.js
@@ -319,5 +319,11 @@ router.post("/sendMail", async (req, res, next) => {
         res.status(200).json("Email has been sent!");
     }
 });
+router.post("/portfolio", async (request,response, next)=>{
+    const url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=ZAR&ids="+ request.body.coin_id+"&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=1h";
+    const obj = await  userFunctions.getPrices(url);
 
+    response.status(200).json(obj);
+
+});
 module.exports = router

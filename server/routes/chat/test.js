4f192c3ebc90891d7591d4495bf313651808824c
Bheki Ndhlovu <bhekindhlovu7@gmail.com>
2021-10-03 21:02:20 +0200

    tag system

--- server/routes/chat/test.js
+++ server/routes/chat/test.js
@@ -1,9 +1,29 @@
-function removeDuplicateCharacters(string) {
-    return string
-        .split('')
-        .filter(function(item, pos, self) {
-            return self.indexOf(item) == pos;
-        })
-        .join('');
+const keyword_extractor = require("keyword-extractor");
+const fetch = require('crypto-symbol');
+
+function keywords(sentence) {
+    let words = sentence.split(' ');
+    let allNames = fetch.getNames();
+    let allSymbols = fetch.getSymbols();
+
+    let res = keyword_extractor.extract(sentence, {
+        language: "english",
+        remove_digits: true,
+        return_changed_case: true,
+        remove_duplicates: false
+    });
+    for(let x = 0; x < words.length; x++) {
+        for(let i = 0; i < allNames.length; i++)
+        {
+            if(words[x].toLowerCase() === allNames[i].toLowerCase())
+            {
+                res.unshift(allNames[i].toLowerCase());
+            }
+
+        }
+    }
+    let unique = res.filter((v, i, a) => a.indexOf(v) === i);
+  return unique;
 }
-console.log(removeDuplicateCharacters('computer security'));
+
+console.log(keywords("i hate nagacoin because of the government regulations but uei is good"));

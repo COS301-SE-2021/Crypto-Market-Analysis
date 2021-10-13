ec0f4d44dacb81418fb991ecd4f4a2b9e8b18512
MelussiMkhonta <u14079412@tuks.co.za>
2021-10-14 00:20:25 +0200

    removed unused imports and spaces

--- client/src/Pages/UpdatePassword/updatePassword.js
+++ client/src/Pages/UpdatePassword/updatePassword.js
@@ -2,7 +2,6 @@ import React, { useRef, useState } from "react"
 import { Form, Button, Card, Alert } from "react-bootstrap"
 import { useAuth } from "../../Auth/Auth"
 import { Link, useHistory } from "react-router-dom"
-import Buttons from "@material-ui/core/Button"
 
 export default function UpdatePassword() {
     const emailRef = useRef()

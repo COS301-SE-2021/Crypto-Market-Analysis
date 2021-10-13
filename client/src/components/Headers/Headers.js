8e3b1afa1707329b08632be768623e5162d9ad06
MelussiMkhonta <u14079412@tuks.co.za>
2021-10-14 00:04:57 +0200

    removed unused imports

--- client/src/components/Headers/Headers.js
+++ client/src/components/Headers/Headers.js
@@ -1,7 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import { makeStyles } from '@material-ui/core/styles'
 import {AppBar, Collapse, IconButton, Toolbar} from "@material-ui/core";
-import SortIcon from '@material-ui/icons/Sort'
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
 import {Link as Scroll} from 'react-scroll'
 import HomeIcon from '@material-ui/icons/Home'
